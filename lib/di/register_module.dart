999dccd508a4c92f77d74f3cf7d54a716598be1e
David Michael II <knowdavidmichael@gmail.com>
2022-06-25 00:45:21 +0100

    config: update register_modeule.dart
    Resgister InternetConnectionChecker lazy singleton,
    Register SharedPrefrences instance

---
+++ lib/di/register_module.dart
@@ -0,0 +1,17 @@
+import 'dart:html';
+
+import 'package:injectable/injectable.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+@module
+abstract class RegisterModule {
+  @lazySingleton
+  InternetConnectionChecker get connection => InternetConnectionChecker();
+
+  @preResolve
+  @lazySingleton
+  Future<SharedPreferences> get getPreferences async {
+    return await SharedPreferences.getInstance();
+  }
+}
