54aab73688675ca2d6490b5573dd712db64ff671
David Michael II <knowdavidmichael@gmail.com>
2022-06-25 01:20:07 +0100

    feat: add type to geo_point_converter fromJson

--- lib/core/converters/geo_point_converter.dart
+++ lib/core/converters/geo_point_converter.dart
@@ -7,8 +7,8 @@ class GeoPointConverter
 
   @override
   GeoPoint fromJson(Map<String, dynamic> json) => GeoPoint(
-        json['latitude'],
-        json['longitude'],
+        (json['latitude'] as num).toDouble(),
+        (json['longitude'] as num).toDouble(),
       );
 
   @override
