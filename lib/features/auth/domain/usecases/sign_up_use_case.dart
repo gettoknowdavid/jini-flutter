3791633f451d85f17dca01ebf3810bfe460d6008
David Michael II <knowdavidmichael@gmail.com>
2022-06-25 03:26:14 +0100

    refactor: sign_up_params

--- lib/features/auth/domain/usecases/sign_up_use_case.dart
+++ lib/features/auth/domain/usecases/sign_up_use_case.dart
@@ -1,10 +1,14 @@
+import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
+import 'package:jini/core/enums/gender.dart';
+import 'package:jini/core/enums/user_type.dart';
 import 'package:jini/core/error/failure.dart';
 import 'package:dartz/dartz.dart';
-import 'package:jini/core/usecase/sign_up_params.dart';
 import 'package:jini/core/usecase/use_case.dart';
 import 'package:jini/features/auth/app/auth_result.dart';
 import 'package:jini/features/auth/domain/repositories/auth_repository.dart';
 
+@lazySingleton
 class SignUpUseCase implements UseCase<AuthResult?, SignUpParams> {
   SignUpUseCase(this.repository);
 
@@ -20,4 +24,23 @@ class SignUpUseCase implements UseCase<AuthResult?, SignUpParams> {
       params.gender,
     );
   }
+}
+
+class SignUpParams extends Equatable {
+  SignUpParams({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.userType,
+    required this.gender,
+  });
+
+  final String name;
+  final String email;
+  final String password;
+  final UserType userType;
+  final Gender gender;
+
+  @override
+  List<Object> get props => [name, email, password, userType, gender];
 }
