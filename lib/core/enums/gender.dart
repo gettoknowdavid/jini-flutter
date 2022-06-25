3a961240fc51833ed9b0112a9bf08ea5cdeae4b8
David Michael II <knowdavidmichael@gmail.com>
2022-06-25 01:00:36 +0100

    feat: create gender enum

---
+++ lib/core/enums/gender.dart
@@ -0,0 +1,20 @@
+enum Gender { male, female, other }
+
+extension GenderExtension on Gender {
+  String get value {
+    switch (this) {
+      case Gender.male:
+        return 'Male';
+      case Gender.female:
+        return 'Female';
+      default:
+        return 'Other';
+    }
+  }
+}
+
+const $GenderEnum = {
+  Gender.male: 'male',
+  Gender.female: 'female',
+  Gender.other: 'other',
+};
