a1b1ef438b88ab2ea9059442e4b7786bc3711a05
David Michael II <knowdavidmichael@gmail.com>
2022-06-25 03:27:44 +0100

    feat: update sign_in_use_case.dart
    Add LazySingleton instance

--- lib/features/auth/domain/usecases/sign_in_use_case.dart
+++ lib/features/auth/domain/usecases/sign_in_use_case.dart
@@ -1,10 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:injectable/injectable.dart';
 import 'package:jini/core/error/failure.dart';
 import 'package:jini/core/usecase/use_case.dart';
 import 'package:jini/features/auth/app/auth_result.dart';
 import 'package:jini/features/auth/domain/repositories/auth_repository.dart';
 
+@lazySingleton
 class SignInUseCase implements UseCase<AuthResult?, SignInParams> {
   SignInUseCase(this.repository);
 
