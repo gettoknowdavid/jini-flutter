9b1d6688925f4147a959c269d19b4657a9488d98
David Michael II <knowdavidmichael@gmail.com>
2022-06-26 03:36:20 +0100

    feat: create auth value_objects

---
+++ lib/src/domain/auth/value_objects.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:jini/src/domain/core/failures.dart';
+import 'package:jini/src/domain/core/value_object.dart';
+import 'package:jini/src/domain/core/value_validators.dart';
+
+
+class EmailAddress extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
