a4c263cfa907358058cb12d85df616dd1b78854b
David Michael II <knowdavidmichael@gmail.com>
2022-06-26 16:29:28 +0100

    Update value_object.dart

--- lib/src/domain/core/value_object.dart
+++ lib/src/domain/core/value_object.dart
@@ -1,11 +1,18 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:jini/src/domain/core/errors.dart';
 import 'package:jini/src/domain/core/failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   Either<ValueFailure<T>, T> get value;
   const ValueObject();
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure]
+  // id = identity - same as writing (right) => right
+  T getOrCrash() => value.fold((l) => throw UnexpectedValueError(l), id);
+
+  bool isValid() => value.isRight();
 
   @override
   bool operator ==(Object o) {
