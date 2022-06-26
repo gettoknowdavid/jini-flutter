5712b1ba04fdd3f3ca4b1e7296b3244c292f1781
David Michael II <knowdavidmichael@gmail.com>
2022-06-26 16:30:06 +0100

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:jini/main.dart';
+import 'package:jini/src/presentation/core/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const JiniApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
