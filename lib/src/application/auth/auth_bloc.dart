aeea06cc07748bc12450036e9848ea18adb04dc5
David Michael II <knowdavidmichael@gmail.com>
2022-06-26 16:28:07 +0100

    feat: create auth_bloc

---
+++ lib/src/application/auth/auth_bloc.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+part 'auth_bloc.freezed.dart';
+
+@injectable
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(_Initial()) {
+    on<AuthEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
