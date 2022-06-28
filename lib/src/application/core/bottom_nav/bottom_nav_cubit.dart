f34c9ae0e9bdbfe933f8a54ea089bf3f811394f4
David Michael II <knowdavidmichael@gmail.com>
2022-06-28 21:52:06 +0100

    Update bottom_nav_cubit.dart

--- lib/src/application/core/bottom_nav/bottom_nav_cubit.dart
+++ lib/src/application/core/bottom_nav/bottom_nav_cubit.dart
@@ -1,29 +1,50 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:jini/src/presentation/core/app.dart';
-
-part 'bottom_nav_state.dart';
-part 'bottom_nav_cubit.freezed.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 @lazySingleton
-class BottomNavCubit extends Cubit<BottomNavState> {
-  BottomNavCubit() : super(BottomNavState(view: MyHomePage(title: '')));
+class BottomNavCubit extends Cubit<Widget> {
+  BottomNavCubit(Widget view) : super(view);
 
-  void getNavBarItem(int index) {
+  int currentIndex = 0;
+
+  void setView(int index) {
+    currentIndex = index;
     switch (index) {
       case 0:
-        emit(BottomNavState(view: MyHomePage(title: 'Home')));
+        emit(MyHomePage(title: 'Home'));
         break;
       case 1:
-        emit(BottomNavState(view: MyHomePage(title: 'Add Request')));
+        emit(ProfilePage(title: 'Profile'));
         break;
       case 2:
-        emit(BottomNavState(view: MyHomePage(title: 'Settings')));
+        emit(ProfilePage(title: 'Make Requet'));
+        break;
+      case 3:
+        emit(ProfilePage(title: 'Events'));
+        break;
+      case 4:
+        emit(ProfilePage(title: 'About'));
         break;
       default:
-        emit(BottomNavState(view: MyHomePage(title: 'Home')));
+        emit(ProfilePage(title: 'Home'));
     }
   }
+
+  List<BottomNavItem> navItems = [
+    BottomNavItem(title: 'Home', icon: PhosphorIcons.houseSimple),
+    BottomNavItem(title: 'Profile', icon: PhosphorIcons.user),
+    BottomNavItem(title: 'Make Request', icon: PhosphorIcons.drop),
+    BottomNavItem(title: 'Events', icon: PhosphorIcons.calendar),
+    BottomNavItem(title: 'About', icon: PhosphorIcons.info),
+  ];
+}
+
+class BottomNavItem {
+  final String title;
+  final IconData icon;
+
+  BottomNavItem({required this.title, required this.icon});
 }
