04e772b10e3c571c6c61afed9de2e82ccc32b896
David Michael II <knowdavidmichael@gmail.com>
2022-06-28 19:39:14 +0100

    feat: add bloc builder to layout widget

--- lib/src/presentation/core/layout/layout.dart
+++ lib/src/presentation/core/layout/layout.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jini/src/application/core/bottom_nav/bottom_nav_cubit.dart';
 
 class Layout extends StatelessWidget {
   const Layout({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<BottomNavCubit, BottomNavState>(
+      builder: (context, state) {
+        return Scaffold();
+      },
+    );
   }
 }
