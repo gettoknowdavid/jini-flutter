3d138dc65d12627ec44b791e9d555a02dee94f17
David Michael II <knowdavidmichael@gmail.com>
2022-06-29 23:22:17 +0100

    feat: add bloc consumer to sign_in_form

--- lib/src/presentation/auth/sign_in/widgets/sign_in_form.dart
+++ lib/src/presentation/auth/sign_in/widgets/sign_in_form.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:jini/src/application/auth/sign_in/sign_in_bloc.dart';
 import 'package:jini/src/presentation/core/j_button.dart';
 import 'package:jini/src/presentation/core/j_text_form_field.dart';
 
@@ -9,33 +11,44 @@ class SignInForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          JTextFormField(
-            hint: 'Email address',
-            keyboardType: TextInputType.emailAddress,
-          ),
-          20.verticalSpace,
-          JTextFormField(hint: 'Password', isPassword: true),
-          16.verticalSpace,
-          GestureDetector(
-            onTap: () {},
-            child: Text(
-              'Forgot Password?',
-              textAlign: TextAlign.right,
-              style: GoogleFonts.spaceGrotesk(
-                color: Colors.white60,
-                fontWeight: FontWeight.w500,
-                fontSize: 14.sp,
+    final bloc = BlocProvider.of<SignInBloc>(context);
+
+    return BlocConsumer<SignInBloc, SignInState>(
+      listener: (context, state) {
+        // TODO: implement listener
+      },
+      builder: (context, state) {
+        return Form(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              JTextFormField(
+                hint: 'Email address',
+                keyboardType: TextInputType.emailAddress,
+                onChanged: (value) => bloc.add(SignInEvent.emailChanged(value)),
               ),
-            ),
+              20.verticalSpace,
+              JTextFormField(hint: 'Password', isPassword: true),
+              16.verticalSpace,
+              GestureDetector(
+                onTap: () {},
+                child: Text(
+                  'Forgot Password?',
+                  textAlign: TextAlign.right,
+                  style: GoogleFonts.spaceGrotesk(
+                    color: Colors.white60,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14.sp,
+                  ),
+                ),
+              ),
+              40.verticalSpace,
+              JButton(title: 'Sign In', onPressed: () {}),
+            ],
           ),
-          40.verticalSpace,
-          JButton(title: 'Sign In', onPressed: () {}),
-        ],
-      ),
+        );
+      },
     );
   }
 }
