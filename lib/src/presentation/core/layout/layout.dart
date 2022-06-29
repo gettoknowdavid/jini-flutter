442b01a56875e3e38f42ea6076f9b27609007472
David Michael II <knowdavidmichael@gmail.com>
2022-06-29 12:15:22 +0100

    feat: add initialization timer for splash screen

--- lib/src/presentation/core/layout/layout.dart
+++ lib/src/presentation/core/layout/layout.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:jini/src/application/core/bottom_nav/bottom_nav_cubit.dart';
 import 'package:jini/src/presentation/core/layout/widgets/bottom_nav.dart';
 
-class Layout extends StatelessWidget {
+class Layout extends StatefulWidget {
   const Layout({Key? key}) : super(key: key);
+
+  @override
+  State<Layout> createState() => _LayoutState();
+}
+
+class _LayoutState extends State<Layout> {
+  void initialization() async {
+    await Future.delayed(const Duration(seconds: 5));
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initialization();
+  }
 
   @override
   Widget build(BuildContext context) {
