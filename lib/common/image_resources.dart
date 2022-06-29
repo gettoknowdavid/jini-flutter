cd753a5e739b7411b158690251e956837096e0ff
David Michael II <knowdavidmichael@gmail.com>
2022-06-29 22:07:45 +0100

    feat: add background image resource

--- lib/common/image_resources.dart
+++ lib/common/image_resources.dart
@@ -1,3 +1,5 @@
 class ImageResources {
   static final String logo = "assets/images/logo.png";
+  static final String background = "assets/images/background.png";
+  static final String splashBackground = "assets/images/splash-background.png";
 }
