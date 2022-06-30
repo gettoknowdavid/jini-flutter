89c8f3c0bb9139d9bdd3a7890f2184bf82381741
David Michael II <knowdavidmichael@gmail.com>
2022-06-30 22:53:08 +0100

    feat: add UserTypeExtension to user_type enum

--- lib/core/enums/user_type.dart
+++ lib/core/enums/user_type.dart
@@ -1,5 +1,18 @@
 enum UserType { donor, recipient }
 
+extension UserTypeExtension on UserType {
+  String get value {
+    switch (this) {
+      case UserType.donor:
+        return 'Donor';
+      case UserType.recipient:
+        return 'Recipient';
+      default:
+        return '';
+    }
+  }
+}
+
 const $UserTypeEnumMap = {
   UserType.donor: 'donor',
   UserType.recipient: 'recipient',
