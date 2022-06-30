ff40112631088d84732d27570ac3e54873b7bd11
David Michael II <knowdavidmichael@gmail.com>
2022-06-30 23:11:04 +0100

    feat: create j_back_button

---
+++ lib/src/presentation/core/j_back_button.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class JBackButton extends StatelessWidget {
+  const JBackButton({Key? key, this.color}) : super(key: key);
+
+  final Color? color;
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: Icon(PhosphorIcons.arrowBendUpLeftBold),
+      color: color,
+      tooltip: MaterialLocalizations.of(context).backButtonTooltip,
+      onPressed: () => Get.back(),
+    );
+  }
+}
