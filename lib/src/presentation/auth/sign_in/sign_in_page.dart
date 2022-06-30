bea9da004525a984719c245212b44f7e9a94b0c4
David Michael II <knowdavidmichael@gmail.com>
2022-06-30 22:14:16 +0100

    fix: sign_in_page

--- lib/src/presentation/auth/sign_in/sign_in_page.dart
+++ lib/src/presentation/auth/sign_in/sign_in_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
-import 'package:get/route_manager.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jini/common/app_routes.dart';
 import 'package:jini/common/image_resources.dart';
