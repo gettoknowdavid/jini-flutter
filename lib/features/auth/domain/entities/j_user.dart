5086e28cd5a057695107da5b13d171e47cf265c8
David Michael II <knowdavidmichael@gmail.com>
2022-06-30 21:42:40 +0100

    feat: update app

--- lib/features/auth/domain/entities/j_user.dart
+++ lib/features/auth/domain/entities/j_user.dart
@@ -1,94 +1,94 @@
-import 'package:equatable/equatable.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:jini/common/models/geo.dart';
-import 'package:jini/core/enums/blood_group.dart';
-import 'package:jini/core/enums/gender.dart';
-import 'package:jini/core/enums/user_type.dart';
+// import 'package:equatable/equatable.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:jini/common/models/geo.dart';
+// import 'package:jini/core/enums/blood_group.dart';
+// import 'package:jini/core/enums/gender.dart';
+// import 'package:jini/core/enums/user_type.dart';
 
-class JUser extends Equatable {
-  const JUser({
-    required this.uid,
-    this.name,
-    this.gender,
-    this.age,
-    this.height,
-    this.weight,
-    required this.email,
-    this.phone,
-    this.city,
-    this.location,
-    this.bloodGroup,
-    required this.userType,
-    this.eligible,
-    this.formComplete,
-    this.initEdit,
-  });
+// class JUser extends Equatable {
+//   const JUser({
+//     required this.uid,
+//     this.name,
+//     this.gender,
+//     this.age,
+//     this.height,
+//     this.weight,
+//     required this.email,
+//     this.phone,
+//     this.city,
+//     this.location,
+//     this.bloodGroup,
+//     required this.userType,
+//     this.eligible,
+//     this.formComplete,
+//     this.initEdit,
+//   });
 
-  /// The unique ID of the user / from [FirebaseAuth]
-  final String uid;
+//   /// The unique ID of the user / from [FirebaseAuth]
+//   final String uid;
 
-  /// The name of the user
-  final String? name;
+//   /// The name of the user
+//   final String? name;
 
-  /// The [Gender] of the user
-  final Gender? gender;
+//   /// The [Gender] of the user
+//   final Gender? gender;
 
-  // / The age of the user / Users who are of [UserType.donor] must be 18 years /
-  // and above; [UserType.recipient] users can be of any / age
-  final num? age;
+//   // / The age of the user / Users who are of [UserType.donor] must be 18 years /
+//   // and above; [UserType.recipient] users can be of any / age
+//   final num? age;
 
-  /// The hight of the user
-  final num? height;
+//   /// The hight of the user
+//   final num? height;
 
-  // / The weight of the user. / Users registered as [UserType.donor] must weigh
-  final num? weight;
+//   // / The weight of the user. / Users registered as [UserType.donor] must weigh
+//   final num? weight;
 
-  /// The mobile phone of the user
-  final String email;
+//   /// The mobile phone of the user
+//   final String email;
 
-  /// The mobile phone of the user
-  final String? phone;
+//   /// The mobile phone of the user
+//   final String? phone;
 
-  /// The city of the user
-  final String? city;
+//   /// The city of the user
+//   final String? city;
 
-  /// The current location of the user
-  final Geo? location;
+//   /// The current location of the user
+//   final Geo? location;
 
-  /// The [BloodGroup] of the user
-  final BloodGroup? bloodGroup;
+//   /// The [BloodGroup] of the user
+//   final BloodGroup? bloodGroup;
 
-  // / The [UserType] of the user / Can be a [UserType.donor] or a
-  // [UserType.recipient]
-  final UserType userType;
+//   // / The [UserType] of the user / Can be a [UserType.donor] or a
+//   // [UserType.recipient]
+//   final UserType userType;
 
-  // / Bool to check if user registered as a donor is / eligible to be a donor or
-  // not
-  final bool? eligible;
+//   // / Bool to check if user registered as a donor is / eligible to be a donor or
+//   // not
+//   final bool? eligible;
 
-  // / Returns true if the user registered as a donor /  has filled the donor
-  // requirements form. / Will return false otherwise.
-  final bool? formComplete;
+//   // / Returns true if the user registered as a donor /  has filled the donor
+//   // requirements form. / Will return false otherwise.
+//   final bool? formComplete;
 
-  /// Checks for initial profile edit for each newly register / user
-  final int? initEdit;
+//   /// Checks for initial profile edit for each newly register / user
+//   final int? initEdit;
 
-  @override
-  List<Object?> get props => [
-        uid,
-        name,
-        gender,
-        age,
-        height,
-        weight,
-        email,
-        phone,
-        city,
-        location,
-        bloodGroup,
-        userType,
-        eligible,
-        formComplete,
-        initEdit
-      ];
-}
+//   @override
+//   List<Object?> get props => [
+//         uid,
+//         name,
+//         gender,
+//         age,
+//         height,
+//         weight,
+//         email,
+//         phone,
+//         city,
+//         location,
+//         bloodGroup,
+//         userType,
+//         eligible,
+//         formComplete,
+//         initEdit
+//       ];
+// }
--- lib/features/auth/domain/usecases/sign_up_use_case.dart
+++ lib/features/auth/domain/usecases/sign_up_use_case.dart
@@ -1,46 +1,46 @@
-import 'package:equatable/equatable.dart';
-import 'package:injectable/injectable.dart';
-import 'package:jini/core/enums/gender.dart';
-import 'package:jini/core/enums/user_type.dart';
-import 'package:jini/core/error/failure.dart';
-import 'package:dartz/dartz.dart';
-import 'package:jini/core/usecase/use_case.dart';
-import 'package:jini/features/auth/app/auth_result.dart';
-import 'package:jini/features/auth/domain/repositories/auth_repository.dart';
+// import 'package:equatable/equatable.dart';
+// import 'package:injectable/injectable.dart';
+// import 'package:jini/core/enums/gender.dart';
+// import 'package:jini/core/enums/user_type.dart';
+// import 'package:jini/core/error/failure.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:jini/core/usecase/use_case.dart';
+// import 'package:jini/features/auth/app/auth_result.dart';
+// import 'package:jini/features/auth/domain/repositories/auth_repository.dart';
 
-@lazySingleton
-class SignUpUseCase implements UseCase<AuthResult?, SignUpParams> {
-  SignUpUseCase(this.repository);
+// @lazySingleton
+// class SignUpUseCase implements UseCase<AuthResult?, SignUpParams> {
+//   SignUpUseCase(this.repository);
 
-  final AuthRepository repository;
+//   final AuthRepository repository;
 
-  @override
-  Future<Either<Failure, AuthResult?>> call(SignUpParams params) async {
-    return await repository.signUp(
-      params.email,
-      params.email,
-      params.email,
-      params.userType,
-      params.gender,
-    );
-  }
-}
+//   @override
+//   Future<Either<Failure, AuthResult?>> call(SignUpParams params) async {
+//     return await repository.signUp(
+//       params.email,
+//       params.email,
+//       params.email,
+//       params.userType,
+//       params.gender,
+//     );
+//   }
+// }
 
-class SignUpParams extends Equatable {
-  SignUpParams({
-    required this.name,
-    required this.email,
-    required this.password,
-    required this.userType,
-    required this.gender,
-  });
+// class SignUpParams extends Equatable {
+//   SignUpParams({
+//     required this.name,
+//     required this.email,
+//     required this.password,
+//     required this.userType,
+//     required this.gender,
+//   });
 
-  final String name;
-  final String email;
-  final String password;
-  final UserType userType;
-  final Gender gender;
+//   final String name;
+//   final String email;
+//   final String password;
+//   final UserType userType;
+//   final Gender gender;
 
-  @override
-  List<Object> get props => [name, email, password, userType, gender];
-}
+//   @override
+//   List<Object> get props => [name, email, password, userType, gender];
+// }
