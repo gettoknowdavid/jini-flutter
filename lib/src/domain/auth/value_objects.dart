71ef3aa71f69dbfba3b669960f623b6e492972ec
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 01:49:09 +0100

    feat: add name, gender and usertype value_objects

--- lib/src/domain/auth/value_objects.dart
+++ lib/src/domain/auth/value_objects.dart
@@ -1,8 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:jini/src/domain/core/failures.dart';
+import 'package:jini/src/domain/core/gender.dart';
+import 'package:jini/src/domain/core/user_type.dart';
 import 'package:jini/src/domain/core/value_object.dart';
 import 'package:jini/src/domain/core/value_validators.dart';
-
 
 class EmailAddress extends ValueObject<String> {
   final Either<ValueFailure<String>, String> value;
@@ -26,4 +27,34 @@ class Password extends ValueObject<String> {
   }
 
   const Password._(this.value);
+}
+
+class IName extends ValueObject<String> {
+  final Either<ValueFailure<String>, String> value;
+
+  factory IName(String input) {
+    return IName._(right(input));
+  }
+
+  const IName._(this.value);
+}
+
+class IGender extends ValueObject<Gender> {
+  final Either<ValueFailure<Gender>, Gender> value;
+
+  factory IGender(Gender input) {
+    return IGender._(right(input));
+  }
+
+  const IGender._(this.value);
+}
+
+class IUserType extends ValueObject<UserType> {
+  final Either<ValueFailure<UserType>, UserType> value;
+
+  factory IUserType(UserType input) {
+    return IUserType._(right(input));
+  }
+
+  const IUserType._(this.value);
 }
