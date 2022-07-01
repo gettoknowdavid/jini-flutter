11aa63b21c574b084dee89f87bbed65ab22f1aaa
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 17:04:41 +0100

    Update value_validators.dart

--- lib/src/domain/core/value_validators.dart
+++ lib/src/domain/core/value_validators.dart
@@ -1,6 +1,14 @@
 import 'package:dartz/dartz.dart';
 import 'package:jini/src/domain/core/failures.dart';
 
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isEmpty) {
+    return left(ValueFailure.empty(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const emailRegex =
       r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
@@ -8,6 +16,15 @@ Either<ValueFailure<String>, String> validateEmailAddress(String input) {
     return right(input);
   } else {
     return left(ValueFailure.invalidEmail(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validatePhone(String input) {
+  const phoneRegex = r"""^0[0-9]\d{0,12}$""";
+  if (RegExp(phoneRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidPhone(failedValue: input));
   }
 }
 
@@ -21,5 +38,25 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
     return right(input);
   } else {
     return left(ValueFailure.invalidPassword(failedValue: input));
+  }
+}
+
+Either<ValueFailure<num>, num> validateAge(num input) {
+  if (input.toInt() < 17) {
+    return left(ValueFailure.tooYoung(failedValue: input));
+  } else if (input.toInt() > 60) {
+    return left(ValueFailure.tooOld(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+Either<ValueFailure<num>, num> validateWeight(num input) {
+  if (input < 45) {
+    return left(ValueFailure.weightTooSmall(failedValue: input));
+  } else if (input > 150) {
+    return left(ValueFailure.weightTooBig(failedValue: input));
+  } else {
+    return right(input);
   }
 }
