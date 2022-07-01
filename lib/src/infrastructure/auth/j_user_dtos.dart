cb28da34fd57c462125cb199d859fbbf4b73641e
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 17:04:56 +0100

    Create j_user_dtos.dart

---
+++ lib/src/infrastructure/auth/j_user_dtos.dart
@@ -0,0 +1,61 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:jini/src/domain/auth/j_user.dart';
+import 'package:jini/src/domain/core/blood_group.dart';
+import 'package:jini/src/domain/core/gender.dart';
+import 'package:jini/src/domain/core/geo.dart';
+import 'package:jini/src/domain/core/user_type.dart';
+
+part 'j_user_dtos.freezed.dart';
+part 'j_user_dtos.g.dart';
+
+@freezed
+@JsonSerializable(explicitToJson: true)
+class JUserDto with _$JUserDto {
+  factory JUserDto({
+    required String uid,
+    required String name,
+    Gender? gender,
+    num? age,
+    num? height,
+    num? weight,
+    required String email,
+    String? phone,
+    String? city,
+    Geo? location,
+    required BloodGroup bloodGroup,
+    required UserType userType,
+    bool? eligible,
+    bool? formComplete,
+    bool? initEdit,
+  }) = _JUserDto;
+
+  factory JUserDto.fromDomain(JUser jUser) {
+    return JUserDto(
+      uid: jUser.uid.getOrCrash(),
+      name: jUser.name.getOrCrash(),
+      gender: jUser.gender != null ? jUser.gender!.getOrCrash() : null,
+      age: jUser.age != null ? jUser.age!.getOrCrash() : null,
+      height: jUser.height != null ? jUser.height!.getOrCrash() : null,
+      weight: jUser.weight != null ? jUser.weight!.getOrCrash() : null,
+      email: jUser.email.getOrCrash(),
+      phone: jUser.phone != null ? jUser.phone!.getOrCrash() : null,
+      city: jUser.city,
+      location: jUser.location,
+      bloodGroup: jUser.bloodGroup.getOrCrash(),
+      userType: jUser.userType.getOrCrash(),
+      eligible: jUser.eligible,
+      formComplete: jUser.formComplete,
+      initEdit: jUser.initEdit,
+    );
+  }
+
+  factory JUserDto.fromJson(Map<String, Object?> json) =>
+      _$JUserDtoFromJson(json);
+
+  Map<String, Object?> toJson() => _$JUserDtoToJson(this);
+}
+
+@Collection<JUserDto>('users')
+final jUsersRef = JUserDtoCollectionReference();
