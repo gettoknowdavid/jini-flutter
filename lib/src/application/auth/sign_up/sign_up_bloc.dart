8f49de6fadc33c1bbf27def55440f0a9215372a8
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 17:03:53 +0100

    Update sign_up_bloc.dart

--- lib/src/application/auth/sign_up/sign_up_bloc.dart
+++ lib/src/application/auth/sign_up/sign_up_bloc.dart
@@ -5,7 +5,7 @@ import 'package:injectable/injectable.dart';
 import 'package:jini/src/domain/auth/auth_failure.dart';
 import 'package:jini/src/domain/auth/i_auth_facade.dart';
 import 'package:jini/src/domain/auth/value_objects.dart';
-import 'package:jini/src/domain/core/gender.dart';
+import 'package:jini/src/domain/core/blood_group.dart';
 import 'package:jini/src/domain/core/user_type.dart';
 
 part 'sign_up_event.dart';
@@ -17,6 +17,71 @@ class SignUpBloc extends Bloc<SignUpEvent, SignUpState> {
   final IAuthFacade _authFacade;
 
   SignUpBloc(this._authFacade) : super(SignUpState.initial()) {
-    on<SignUpEvent>((event, emit) {});
+    on<NameChanged>((event, emit) => _nameChanged(event, emit));
+    on<EmailChanged>((event, emit) => _emailChanged(event, emit));
+    on<PasswordChanged>((event, emit) => _passwordChanged(event, emit));
+    on<BloodGroupChanged>((event, emit) => _bloodGroupChanged(event, emit));
+    on<UserTypeChanged>((event, emit) => _userTypeChanged(event, emit));
+    on<SignUpPressed>((event, emit) => _signUpPressed(event, emit));
+  }
+
+  _nameChanged(NameChanged e, Emitter<SignUpState> emit) async {
+    emit(state.copyWith(name: IName(e.name), authFailureOrSuccess: none()));
+  }
+
+  _emailChanged(EmailChanged e, Emitter<SignUpState> emit) async {
+    emit(state.copyWith(
+      email: IEmailAddress(e.email),
+      authFailureOrSuccess: none(),
+    ));
+  }
+
+  _passwordChanged(PasswordChanged e, Emitter<SignUpState> emit) async {
+    emit(state.copyWith(
+      password: IPassword(e.password),
+      authFailureOrSuccess: none(),
+    ));
+  }
+
+  _bloodGroupChanged(BloodGroupChanged e, Emitter<SignUpState> emit) async {
+    emit(state.copyWith(
+      bloodGroup: IBloodGroup(e.bloodGroup),
+      authFailureOrSuccess: none(),
+    ));
+  }
+
+  _userTypeChanged(UserTypeChanged e, Emitter<SignUpState> emit) async {
+    emit(state.copyWith(
+      userType: IUserType(e.userType),
+      authFailureOrSuccess: none(),
+    ));
+  }
+
+  _signUpPressed(SignUpPressed e, Emitter<SignUpState> emit) async {
+    Either<AuthFailure, Unit> _r;
+
+    final isNameValid = state.name.isValid();
+    final isEmailValid = state.email.isValid();
+    final isPasswordValid = state.password.isValid();
+
+    if (isNameValid && isEmailValid && isPasswordValid) {
+      emit(state.copyWith(isSubmitting: true, authFailureOrSuccess: none()));
+
+      _r = await _authFacade.signUp(
+        name: state.name,
+        email: state.email,
+        password: state.password,
+        bloodGroup: state.bloodGroup,
+        userType: state.userType,
+      );
+
+      emit(state.copyWith(isSubmitting: false, authFailureOrSuccess: some(_r)));
+    }
+
+    emit(state.copyWith(
+      isSubmitting: false,
+      showErrorMessages: true,
+      authFailureOrSuccess: none(),
+    ));
   }
 }
