624c0cec270b45bcf8d9c7ad355f876edb26b0c8
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 02:04:28 +0100

    feat: update

--- lib/src/application/auth/sign_up/sign_up_state.dart
+++ lib/src/application/auth/sign_up/sign_up_state.dart
@@ -1,24 +1,24 @@
 part of 'sign_up_bloc.dart';
 
 @freezed
-abstract class SignUpState with _$SignUpState {
+class SignUpState with _$SignUpState {
   const factory SignUpState({
-    required String name,
-    required EmailAddress email,
-    required Password password,
-    required Gender gender,
-    required UserType userType,
+    required IName name,
+    required IEmailAddress email,
+    required IPassword password,
+    required IGender gender,
+    required IUserType userType,
     required bool showErrorMessages,
     required bool isSubmitting,
     required Option<Either<AuthFailure, Unit>> authFailureOrSuccess,
   }) = _SignUpState;
 
   factory SignUpState.initial() => SignUpState(
-        name: '',
-        email: EmailAddress(''),
-        password: Password(''),
-        gender: Gender.other,
-        userType: UserType.donor,
+        name: IName(''),
+        email: IEmailAddress(''),
+        password: IPassword(''),
+        gender: IGender(Gender.other),
+        userType: IUserType(UserType.donor),
         showErrorMessages: false,
         isSubmitting: false,
         authFailureOrSuccess: none(),
