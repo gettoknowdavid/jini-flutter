84287b5b87664520bb412323d139cdf046f7a561
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 21:46:37 +0100

    Update sign_up_event.dart

--- lib/src/application/auth/sign_up/sign_up_event.dart
+++ lib/src/application/auth/sign_up/sign_up_event.dart
@@ -8,7 +8,7 @@ class SignUpEvent with _$SignUpEvent {
 
   const factory SignUpEvent.passwordChanged(String password) = PasswordChanged;
 
-  const factory SignUpEvent.genderChanged(Gender gender) = GenderChanged;
+  const factory SignUpEvent.genderChanged(Gender? gender) = GenderChanged;
 
   const factory SignUpEvent.bloodGroupChanged(BloodGroup bloodGroup) =
       BloodGroupChanged;
