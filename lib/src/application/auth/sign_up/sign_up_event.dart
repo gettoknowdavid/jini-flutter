aa4cb200259f2913712422297d4b546b9bcd5720
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 17:04:01 +0100

    Update sign_up_event.dart

--- lib/src/application/auth/sign_up/sign_up_event.dart
+++ lib/src/application/auth/sign_up/sign_up_event.dart
@@ -2,9 +2,17 @@ part of 'sign_up_bloc.dart';
 
 @freezed
 class SignUpEvent with _$SignUpEvent {
+  const factory SignUpEvent.nameChanged(String name) = NameChanged;
+
   const factory SignUpEvent.emailChanged(String email) = EmailChanged;
 
   const factory SignUpEvent.passwordChanged(String password) = PasswordChanged;
+
+  const factory SignUpEvent.bloodGroupChanged(BloodGroup bloodGroup) =
+      BloodGroupChanged;
+
+  const factory SignUpEvent.userTypeChanged(UserType userType) =
+      UserTypeChanged;
 
   const factory SignUpEvent.signUpPressed() = SignUpPressed;
 }
