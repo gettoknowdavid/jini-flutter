3658c07ce95deee74a4dfb7786b050c3b87ff533
David Michael II <knowdavidmichael@gmail.com>
2022-07-01 02:04:17 +0100

    feat: update

--- lib/src/application/auth/sign_in/sign_in_state.dart
+++ lib/src/application/auth/sign_in/sign_in_state.dart
@@ -3,16 +3,16 @@ part of 'sign_in_bloc.dart';
 @freezed
 abstract class SignInState with _$SignInState {
   const factory SignInState({
-    required EmailAddress email,
-    required Password password,
+    required IEmailAddress email,
+    required IPassword password,
     required bool showErrorMessages,
     required bool isSubmitting,
     required Option<Either<AuthFailure, Unit>> authFailureOrSuccess,
   }) = _SignInState;
 
   factory SignInState.initial() => SignInState(
-        email: EmailAddress(''),
-        password: Password(''),
+        email: IEmailAddress(''),
+        password: IPassword(''),
         showErrorMessages: false,
         isSubmitting: false,
         authFailureOrSuccess: none(),
