016f3a317eeb6183328d072b1079ba59fa2ef10a
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 00:19:13 +0100

    feat: add emailNotVerified failure

--- lib/src/domain/auth/auth_failure.dart
+++ lib/src/domain/auth/auth_failure.dart
@@ -5,6 +5,7 @@ part 'auth_failure.freezed.dart';
 @freezed
 class AuthFailure with _$AuthFailure {
   const factory AuthFailure.serverError() = ServerError;
+  const factory AuthFailure.emailNotVerified() = EmailNotVerified;
   const factory AuthFailure.emailInUse() = EmailInUse;
   const factory AuthFailure.invalidEmailOrPassword() = InvalidEmailOrPassword;
   const factory AuthFailure.unableToUpdate() = UnableToUpdate;
