e35994beff1b225fa1c87c762b35ef71f41c9c79
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 15:01:15 +0100

    feat: create verification page

---
+++ lib/src/presentation/auth/verification/verification_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class VerificationPage extends StatelessWidget {
+  const VerificationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            Text('Verify your Email'),
+          ],
+        ),
+      ),
+    );
+  }
+}
