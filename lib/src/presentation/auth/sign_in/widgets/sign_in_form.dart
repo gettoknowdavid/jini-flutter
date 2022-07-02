e797668d781cb8ba0edca6ddbfe17d536a9e5508
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 15:00:48 +0100

    feat: add routing after sign in auth

--- lib/src/presentation/auth/sign_in/widgets/sign_in_form.dart
+++ lib/src/presentation/auth/sign_in/widgets/sign_in_form.dart
@@ -4,9 +4,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jini/common/app_colors.dart';
+import 'package:jini/src/application/auth/auth_bloc.dart';
 import 'package:jini/src/application/auth/sign_in/sign_in_bloc.dart';
 import 'package:jini/src/presentation/core/j_button.dart';
 import 'package:jini/src/presentation/core/j_text_form_field.dart';
+import 'package:jini/src/presentation/routes/j_router.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class SignInForm extends StatelessWidget {
@@ -15,24 +17,31 @@ class SignInForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final bloc = BlocProvider.of<SignInBloc>(context);
+    final authBloc = BlocProvider.of<AuthBloc>(context);
 
     return BlocConsumer<SignInBloc, SignInState>(
       listener: (context, state) {
         state.authFailureOrSuccess.fold(
           () {},
-          (either) => either.fold((f) {
-            Get.snackbar(
-              'Authentication Failure',
-              f.maybeMap(
-                orElse: () => '',
-                serverError: (_) => 'Looks like there\'s a server error.',
-                invalidEmailOrPassword: (_) => 'Invalid email or password.',
-              ),
-              icon: Icon(PhosphorIcons.warningCircleBold),
-              snackPosition: SnackPosition.BOTTOM,
-              backgroundColor: AppColors.primary,
-            );
-          }, (r) => null),
+          (either) => either.fold(
+            (f) {
+              Get.snackbar(
+                'Authentication Failure',
+                f.maybeMap(
+                  orElse: () => '',
+                  serverError: (_) => 'Looks like there\'s a server error.',
+                  invalidEmailOrPassword: (_) => 'Invalid email or password.',
+                ),
+                icon: Icon(PhosphorIcons.warningCircleBold),
+                snackPosition: SnackPosition.BOTTOM,
+                backgroundColor: AppColors.primary,
+              );
+            },
+            (_) {
+              Get.offAllNamed(JRoutes.layout);
+              authBloc.add(const AuthEvent.authCheckRequested());
+            },
+          ),
         );
       },
       builder: (context, state) {
