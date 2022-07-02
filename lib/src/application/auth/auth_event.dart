8776323619f1a91a544e1f186163c1d20cc9259d
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 21:39:05 +0100

    Update auth_event.dart

--- lib/src/application/auth/auth_event.dart
+++ lib/src/application/auth/auth_event.dart
@@ -3,7 +3,7 @@ part of 'auth_bloc.dart';
 @freezed
 class AuthEvent with _$AuthEvent {
   const factory AuthEvent.authCheckRequested() = _AuthCheckRequested;
-  const factory AuthEvent.reload() = _AuthReload;
-  const factory AuthEvent.sendEmailVerification() = _SendEmailVerification;
+  const factory AuthEvent.authCheckVerified() = _AuthCheckVerified;
+  const factory AuthEvent.sendVerifiedEmail() = _SendVerifiedEmail;
   const factory AuthEvent.authSignedOut() = _SignedOut;
 }
