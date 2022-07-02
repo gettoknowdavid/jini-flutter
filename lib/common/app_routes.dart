094bc9ab7f7eeacc578c4f8b7efe420c0935d592
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 12:20:56 +0100

    Update app_routes.dart

--- lib/common/app_routes.dart
+++ lib/common/app_routes.dart
@@ -1,5 +1,5 @@
-class AppRoutes {
-  static final String root = "/";
-  static final String signIn = "/signIn";
-  static final String signUp = "/signUp";
-}
+// class AppRoutes {
+//   static final String root = "/";
+//   static final String signIn = "/signIn";
+//   static final String signUp = "/signUp";
+// }
