4921a7bcbddb06a19249f3b2664fb75804ef30ab
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 21:39:00 +0100

    Update auth_bloc.dart

--- lib/src/application/auth/auth_bloc.dart
+++ lib/src/application/auth/auth_bloc.dart
@@ -13,6 +13,8 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
 
   AuthBloc(this._authFacade) : super(_Initial()) {
     on<_AuthCheckRequested>((event, emit) => _authCheckRequested(event, emit));
+    on<_AuthCheckVerified>((event, emit) => _checkVerified(event, emit));
+    on<_SendVerifiedEmail>((event, emit) => _sendVerifiedEmail(event, emit));
     on<_SignedOut>((event, emit) => _signedOut(event, emit));
   }
 
@@ -24,6 +26,21 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
         (_) => const AuthState.authenticated(),
       ),
     );
+  }
+
+  _checkVerified(_AuthCheckVerified e, Emitter<AuthState> emit) async {
+    final _verifiedOption = await _authFacade.checkEmailVerified();
+    emit(
+      _verifiedOption.fold(
+        () => const AuthState.unverified(),
+        (_) => const AuthState.verified(),
+      ),
+    );
+  }
+
+  _sendVerifiedEmail(_SendVerifiedEmail e, Emitter<AuthState> emit) async {
+    await _authFacade.sendVerificationEmail();
+    emit(const AuthState.awaitingVerified());
   }
 
   _signedOut(_SignedOut e, Emitter<AuthState> emit) async {
