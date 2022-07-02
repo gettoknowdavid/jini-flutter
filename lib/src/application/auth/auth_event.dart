bb6caf4ea5e07e664533c109093bd32818ff8825
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 15:05:37 +0100

    feat: implement sendVerifcationEmail in FirebaseAuthFacade

--- lib/src/application/auth/auth_event.dart
+++ lib/src/application/auth/auth_event.dart
@@ -3,5 +3,6 @@ part of 'auth_bloc.dart';
 @freezed
 class AuthEvent with _$AuthEvent {
   const factory AuthEvent.authCheckRequested() = _AuthCheckRequested;
+  // const factory AuthEvent.reload() = _AuthReload;
   const factory AuthEvent.authSignedOut() = _SignedOut;
 }
