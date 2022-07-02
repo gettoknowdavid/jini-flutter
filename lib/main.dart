5781de9b94895aaf35e797992ba90c35e31667c0
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 12:21:02 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:injectable/injectable.dart';
 import 'package:jini/di/injection.dart';
 import 'package:jini/firebase_options.dart';
+import 'package:jini/src/application/auth/auth_bloc.dart';
 import 'package:jini/src/application/auth/sign_in/sign_in_bloc.dart';
 import 'package:jini/src/application/auth/sign_up/sign_up_bloc.dart';
 import 'package:jini/src/application/core/bottom_nav/bottom_nav_cubit.dart';
@@ -36,6 +37,9 @@ void main() async {
         BlocProvider<BottomNavCubit>(create: (context) => BottomNavCubit()),
         BlocProvider<SignInBloc>(create: (context) => getIt<SignInBloc>()),
         BlocProvider<SignUpBloc>(create: (context) => getIt<SignUpBloc>()),
+        BlocProvider<AuthBloc>(create: (context) {
+          return getIt<AuthBloc>()..add(AuthEvent.authCheckRequested());
+        }),
       ],
       child: JiniApp(),
     ),
