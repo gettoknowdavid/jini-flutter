607f5bf1e83347c048a9456216c23e298f87cb70
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 21:38:57 +0100

    Update image_resources.dart

--- lib/common/image_resources.dart
+++ lib/common/image_resources.dart
@@ -2,4 +2,5 @@ class ImageResources {
   static const String logo = "assets/images/logo.png";
   static const String background = "assets/images/background.png";
   static const String splashBackground = "assets/images/splash-background.png";
+  static const String verifyEmail = "assets/images/verify-email.png";
 }
