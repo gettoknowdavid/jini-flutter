2e613e7b0efe667b845c5e5c4d00e508c9c83c31
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 12:21:43 +0100

    Update app.dart

--- lib/src/presentation/core/app.dart
+++ lib/src/presentation/core/app.dart
@@ -1,11 +1,14 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:jini/common/app_colors.dart';
-import 'package:jini/common/app_routes.dart';
 import 'package:jini/src/presentation/auth/sign_in/sign_in_page.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jini/src/presentation/auth/sign_up/sign_up_page.dart';
+import 'package:jini/src/presentation/core/layout/j_layout.dart';
+import 'package:jini/src/presentation/routes/j_router.dart';
+import 'package:jini/src/presentation/splash/splash_page.dart';
 
 class JiniApp extends StatelessWidget {
   const JiniApp({Key? key}) : super(key: key);
@@ -13,19 +16,25 @@ class JiniApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = ThemeData.dark();
+    // final _jRouter = getIt<JRouter>();
+
     return ScreenUtilInit(
       designSize: const Size(393, 830),
       minTextAdapt: true,
-      child: const SignInPage(),
+      child: const JLayout(),
       builder: (context, child) {
         return GetMaterialApp(
           title: 'Jini Donation',
-          home: child,
           defaultTransition: Transition.fadeIn,
           transitionDuration: const Duration(milliseconds: 350),
+          home: JLayout(),
+          // routerDelegate: AutoRouterDelegate(_jRouter),
+          // routeInformationParser: _jRouter.defaultRouteParser(),
           getPages: [
-            GetPage(name: AppRoutes.root, page: () => SignInPage()),
-            GetPage(name: AppRoutes.signUp, page: () => SignUpPage()),
+            GetPage(name: JRoutes.layout, page: () => JLayout()),
+            GetPage(name: JRoutes.signIn, page: () => SignInPage()),
+            GetPage(name: JRoutes.signUp, page: () => SignUpPage()),
+            GetPage(name: JRoutes.splash, page: () => SplashPage()),
           ],
           theme: ThemeData(
             primarySwatch: AppColors.swatch,
@@ -37,74 +46,18 @@ class JiniApp extends StatelessWidget {
             progressIndicatorTheme: ProgressIndicatorThemeData(
               color: AppColors.background,
             ),
+            pageTransitionsTheme: PageTransitionsTheme(
+              builders: {
+                TargetPlatform.iOS: NoShadowCupertinoPageTransitionsBuilder(),
+                TargetPlatform.android: ZoomPageTransitionsBuilder(),
+              },
+            ),
             textTheme: theme.textTheme
                 .copyWith()
                 .apply(fontFamily: GoogleFonts.spaceGrotesk().fontFamily),
           ),
         );
       },
-    );
-  }
-}
-
-class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: Center(
-        child: Text(
-          title.toUpperCase(),
-          style: Theme.of(context).textTheme.headline4,
-        ),
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
 }
