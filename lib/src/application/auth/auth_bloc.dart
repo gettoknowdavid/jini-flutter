67814c4646c07d30d81a31be83f248b82c1d588c
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 12:21:08 +0100

    Update auth_bloc.dart

--- lib/src/application/auth/auth_bloc.dart
+++ lib/src/application/auth/auth_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:jini/src/domain/auth/i_auth_facade.dart';
 
 part 'auth_event.dart';
 part 'auth_state.dart';
@@ -8,7 +9,25 @@ part 'auth_bloc.freezed.dart';
 
 @injectable
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
-  AuthBloc() : super(_Initial()) {
-    on<AuthEvent>((event, emit) {});
+  final IAuthFacade _authFacade;
+
+  AuthBloc(this._authFacade) : super(_Initial()) {
+    on<_AuthCheckRequested>((event, emit) => _authCheckRequested(event, emit));
+    on<_SignedOut>((event, emit) => _signedOut(event, emit));
+  }
+
+  _authCheckRequested(_AuthCheckRequested e, Emitter<AuthState> emit) async {
+    final _userOption = await _authFacade.getUser();
+    emit(
+      _userOption.fold(
+        () => const AuthState.unauthenticated(),
+        (_) => const AuthState.authenticated(),
+      ),
+    );
+  }
+
+  _signedOut(_SignedOut e, Emitter<AuthState> emit) async {
+    await _authFacade.signOut();
+    emit(const AuthState.unauthenticated());
   }
 }
