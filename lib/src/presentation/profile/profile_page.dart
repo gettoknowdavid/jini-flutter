06486d77a2f0d444641932a0ceda3e53b8dd01b7
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 12:22:00 +0100

    Create profile_page.dart

---
+++ lib/src/presentation/profile/profile_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jini/src/application/auth/auth_bloc.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<AuthBloc>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title.toUpperCase(),
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            ElevatedButton(
+              onPressed: () => bloc.add(AuthEvent.authSignedOut()),
+              child: Text('Sign Out'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
