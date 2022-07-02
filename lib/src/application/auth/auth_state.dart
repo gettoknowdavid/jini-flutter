a7bf86a43471d343427c84138c4f468fc73c3778
David Michael II <knowdavidmichael@gmail.com>
2022-07-02 15:11:08 +0100

    feat: add verified, unverified states to AuthState

--- lib/src/application/auth/auth_state.dart
+++ lib/src/application/auth/auth_state.dart
@@ -4,5 +4,7 @@ part of 'auth_bloc.dart';
 class AuthState with _$AuthState {
   const factory AuthState.initial() = _Initial;
   const factory AuthState.authenticated() = _Authenticated;
-  const factory AuthState.unauthenticated() = _Unanthenticated;
+  const factory AuthState.unauthenticated() = _Unauthenticated;
+  const factory AuthState.verified() = _Verified;
+  const factory AuthState.unverified() = _Unverified;
 }
