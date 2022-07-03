266d9c410c12195c2c18fbac60f38b628d6c1ce4
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 01:00:10 +0100

    Update auth_bloc.dart

--- lib/src/application/auth/auth_bloc.dart
+++ lib/src/application/auth/auth_bloc.dart
@@ -14,6 +14,9 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc(this._authFacade) : super(_Initial()) {
     on<_AuthCheckRequested>((event, emit) => _authCheckRequested(event, emit));
     on<_AuthCheckVerified>((event, emit) => _authCheckVerified(event, emit));
+    on<_CheckDonorRequirementsMet>(
+      (event, emit) => _checkDonorRequirementsMet(event, emit),
+    );
     on<_SendVerifiedEmail>((event, emit) => _sendVerifiedEmail(event, emit));
     on<_SignedOut>((event, emit) => _signedOut(event, emit));
   }
@@ -34,6 +37,19 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       _verifiedEither.fold(
         (_) => const AuthState.unverified(),
         (_) => const AuthState.verified(),
+      ),
+    );
+  }
+
+  _checkDonorRequirementsMet(
+    _CheckDonorRequirementsMet e,
+    Emitter<AuthState> emit,
+  ) async {
+    final _donorEligbleEither = await _authFacade.donorRequirementsMet();
+    emit(
+      _donorEligbleEither.fold(
+        (_) => const AuthState.donorFormNotCompleted(),
+        (_) => const AuthState.donorFormCompleted(),
       ),
     );
   }
