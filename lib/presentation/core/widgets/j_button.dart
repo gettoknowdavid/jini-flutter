49322ac427ce01838a1312ef8fb514c27683a34e
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 23:19:13 +0100

    Update j_button.dart

--- lib/presentation/core/widgets/j_button.dart
+++ lib/presentation/core/widgets/j_button.dart
@@ -1,27 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:jini/presentation/core/common/j_theme.dart';
 
 class JButton extends StatelessWidget {
   const JButton({
     Key? key,
     required this.title,
     required this.onPressed,
-    this.fontSize,
-    this.backgroundColor,
-    this.textColor = Colors.white,
-    this.padding,
     this.loading = false,
     this.indicatorColor,
   }) : super(key: key);
 
   final String title;
   final void Function()? onPressed;
-  final double? fontSize;
-  final Color? backgroundColor;
-  final Color? textColor;
-  final EdgeInsetsGeometry? padding;
   final bool loading;
   final Color? indicatorColor;
 
