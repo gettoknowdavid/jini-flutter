dbd60d330a8c156d56c75192fa8aced70e644c0e
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 23:02:22 +0100

    refactor: colors to theme

---
+++ lib/presentation/core/widgets/j_snackbars.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class JSnackbars {
+  JSnackbars._();
+
+  static errorSnackbar({required String title, required String message}) {
+    return Get.snackbar(
+      title,
+      message,
+      icon: Icon(PhosphorIcons.warningCircleBold),
+      snackPosition: SnackPosition.BOTTOM,
+      backgroundColor: Colors.red,
+    );
+  }
+}
