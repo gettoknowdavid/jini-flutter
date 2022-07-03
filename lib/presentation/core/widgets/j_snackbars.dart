e2731240c8205780d50539a38e95e368f524b60b
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 23:34:58 +0100

    Update j_snackbars.dart

--- lib/presentation/core/widgets/j_snackbars.dart
+++ lib/presentation/core/widgets/j_snackbars.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class JSnackbars {
   JSnackbars._();
@@ -11,6 +12,7 @@ class JSnackbars {
       message,
       icon: Icon(PhosphorIcons.warningCircleBold),
       snackPosition: SnackPosition.BOTTOM,
+      margin: EdgeInsets.all(18).r,
       backgroundColor: Colors.red,
       duration: const Duration(seconds: 5),
     );
