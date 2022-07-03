41cd66d0f477a0ec3dc9128f429c21249c70e1cc
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 23:02:13 +0100

    refactor: colors to theme

--- lib/presentation/core/widgets/j_back_button.dart
+++ lib/presentation/core/widgets/j_back_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:jini/presentation/core/common/app_colors.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class JBackButton extends StatelessWidget {
@@ -12,7 +11,7 @@ class JBackButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return IconButton(
       icon: Icon(PhosphorIcons.arrowBendUpLeftBold),
-      color: color ?? AppColors.primary,
+      color: color ?? Theme.of(context).primaryColor,
       tooltip: MaterialLocalizations.of(context).backButtonTooltip,
       onPressed: () => Get.back(),
     );
