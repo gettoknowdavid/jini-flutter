6a8a203c5a69a5131863bf77d9e134f604f7bd9a
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 20:08:51 +0100

    Update injection.config.dart

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -12,12 +12,11 @@ import 'package:internet_connection_checker/internet_connection_checker.dart'
     as _i9;
 import 'package:shared_preferences/shared_preferences.dart' as _i12;
 
-import '../src/application/auth/auth_bloc.dart' as _i18;
+import '../src/application/auth/auth_bloc.dart' as _i17;
 import '../src/application/auth/forgot_password/forgot_password_bloc.dart'
-    as _i19;
+    as _i18;
 import '../src/application/auth/sign_in/sign_in_bloc.dart' as _i15;
 import '../src/application/auth/sign_up/sign_up_bloc.dart' as _i16;
-import '../src/application/auth/verification/verification_cubit.dart' as _i17;
 import '../src/application/core/bottom_nav/bottom_nav_cubit.dart' as _i3;
 import '../src/application/profile/profile_bloc.dart' as _i11;
 import '../src/domain/auth/i_auth_facade.dart' as _i13;
@@ -25,9 +24,9 @@ import '../src/domain/mail/i_mail_facade.dart' as _i7;
 import '../src/infrastructure/auth/firebase_auth_facade.dart' as _i14;
 import '../src/infrastructure/auth/firebase_user_mapper.dart' as _i6;
 import '../src/infrastructure/auth/j_user_dtos.dart' as _i10;
-import '../src/infrastructure/core/firebase_injectable_module.dart' as _i20;
+import '../src/infrastructure/core/firebase_injectable_module.dart' as _i19;
 import '../src/infrastructure/mail/open_mail_app_facade.dart' as _i8;
-import 'register_module.dart' as _i21; // ignore_for_file: unnecessary_lambdas
+import 'register_module.dart' as _i20; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -57,15 +56,13 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
       get<_i10.JUserDtoCollectionReference>()));
   gh.factory<_i15.SignInBloc>(() => _i15.SignInBloc(get<_i13.IAuthFacade>()));
   gh.factory<_i16.SignUpBloc>(() => _i16.SignUpBloc(get<_i13.IAuthFacade>()));
-  gh.factory<_i17.VerificationCubit>(() =>
-      _i17.VerificationCubit(get<_i7.IMailFacade>(), get<_i13.IAuthFacade>()));
-  gh.factory<_i18.AuthBloc>(
-      () => _i18.AuthBloc(get<_i13.IAuthFacade>(), get<_i7.IMailFacade>()));
-  gh.factory<_i19.ForgotPasswordBloc>(
-      () => _i19.ForgotPasswordBloc(get<_i13.IAuthFacade>()));
+  gh.factory<_i17.AuthBloc>(
+      () => _i17.AuthBloc(get<_i13.IAuthFacade>(), get<_i7.IMailFacade>()));
+  gh.factory<_i18.ForgotPasswordBloc>(
+      () => _i18.ForgotPasswordBloc(get<_i13.IAuthFacade>()));
   return get;
 }
 
-class _$FirebaseInjectableModule extends _i20.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i19.FirebaseInjectableModule {}
 
-class _$RegisterModule extends _i21.RegisterModule {}
+class _$RegisterModule extends _i20.RegisterModule {}
