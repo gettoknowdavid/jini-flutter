7243a2e56fc0e5ab5bb24dc23b53d0ff4f17db6b
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 01:12:49 +0100

    Update injection.config.dart

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -10,18 +10,19 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 import 'package:internet_connection_checker/internet_connection_checker.dart'
     as _i7;
-import 'package:shared_preferences/shared_preferences.dart' as _i9;
+import 'package:shared_preferences/shared_preferences.dart' as _i10;
 
-import '../src/application/auth/auth_bloc.dart' as _i14;
-import '../src/application/auth/sign_in/sign_in_bloc.dart' as _i12;
-import '../src/application/auth/sign_up/sign_up_bloc.dart' as _i13;
+import '../src/application/auth/auth_bloc.dart' as _i15;
+import '../src/application/auth/sign_in/sign_in_bloc.dart' as _i13;
+import '../src/application/auth/sign_up/sign_up_bloc.dart' as _i14;
 import '../src/application/core/bottom_nav/bottom_nav_cubit.dart' as _i3;
-import '../src/domain/auth/i_auth_facade.dart' as _i10;
-import '../src/infrastructure/auth/firebase_auth_facade.dart' as _i11;
+import '../src/application/profile/profile_bloc.dart' as _i9;
+import '../src/domain/auth/i_auth_facade.dart' as _i11;
+import '../src/infrastructure/auth/firebase_auth_facade.dart' as _i12;
 import '../src/infrastructure/auth/firebase_user_mapper.dart' as _i6;
 import '../src/infrastructure/auth/j_user_dtos.dart' as _i8;
-import '../src/infrastructure/core/firebase_injectable_module.dart' as _i15;
-import 'register_module.dart' as _i16; // ignore_for_file: unnecessary_lambdas
+import '../src/infrastructure/core/firebase_injectable_module.dart' as _i16;
+import 'register_module.dart' as _i17; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -40,19 +41,20 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
       () => registerModule.connection);
   gh.factory<_i8.JUserDtoCollectionReference>(
       () => firebaseInjectableModule.jUserReference);
-  await gh.lazySingletonAsync<_i9.SharedPreferences>(
+  gh.factory<_i9.ProfileBloc>(() => _i9.ProfileBloc());
+  await gh.lazySingletonAsync<_i10.SharedPreferences>(
       () => registerModule.getPreferences,
       preResolve: true);
-  gh.lazySingleton<_i10.IAuthFacade>(() => _i11.FirebaseAuthFacade(
+  gh.lazySingleton<_i11.IAuthFacade>(() => _i12.FirebaseAuthFacade(
       get<_i4.FirebaseAuth>(),
       get<_i6.FirebaseUserMapper>(),
       get<_i8.JUserDtoCollectionReference>()));
-  gh.factory<_i12.SignInBloc>(() => _i12.SignInBloc(get<_i10.IAuthFacade>()));
-  gh.factory<_i13.SignUpBloc>(() => _i13.SignUpBloc(get<_i10.IAuthFacade>()));
-  gh.factory<_i14.AuthBloc>(() => _i14.AuthBloc(get<_i10.IAuthFacade>()));
+  gh.factory<_i13.SignInBloc>(() => _i13.SignInBloc(get<_i11.IAuthFacade>()));
+  gh.factory<_i14.SignUpBloc>(() => _i14.SignUpBloc(get<_i11.IAuthFacade>()));
+  gh.factory<_i15.AuthBloc>(() => _i15.AuthBloc(get<_i11.IAuthFacade>()));
   return get;
 }
 
-class _$FirebaseInjectableModule extends _i15.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i16.FirebaseInjectableModule {}
 
-class _$RegisterModule extends _i16.RegisterModule {}
+class _$RegisterModule extends _i17.RegisterModule {}
