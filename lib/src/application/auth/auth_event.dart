b55d22a7f710475b9f50714d893552f1bec40350
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 01:00:16 +0100

    Update auth_event.dart

--- lib/src/application/auth/auth_event.dart
+++ lib/src/application/auth/auth_event.dart
@@ -4,6 +4,8 @@ part of 'auth_bloc.dart';
 class AuthEvent with _$AuthEvent {
   const factory AuthEvent.authCheckRequested() = _AuthCheckRequested;
   const factory AuthEvent.authCheckVerified() = _AuthCheckVerified;
+  const factory AuthEvent.checkDonorRequirementsMet() =
+      _CheckDonorRequirementsMet;
   const factory AuthEvent.sendVerifiedEmail() = _SendVerifiedEmail;
   const factory AuthEvent.authSignedOut() = _SignedOut;
 }
