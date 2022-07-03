e1ad3ca8d54858b7a05e26c178c459187a4e27b2
David Michael II <knowdavidmichael@gmail.com>
2022-07-03 15:37:14 +0100

    Update auth_state.dart

--- lib/src/application/auth/auth_state.dart
+++ lib/src/application/auth/auth_state.dart
@@ -2,12 +2,12 @@ part of 'auth_bloc.dart';
 
 @freezed
 class AuthState with _$AuthState {
-  const factory AuthState.initial() = _Initial;
-  const factory AuthState.authenticated() = _Authenticated;
-  const factory AuthState.unauthenticated() = _Unauthenticated;
-  const factory AuthState.verified() = _Verified;
-  const factory AuthState.unverified() = _Unverified;
-  const factory AuthState.awaitingVerified() = _AwaitingVerified;
-  const factory AuthState.donorFormCompleted() = _DonorFormCompleted;
-  const factory AuthState.donorFormNotCompleted() = _DonorFormNotCompleted;
+  const factory AuthState.initial() = Initial;
+  const factory AuthState.authenticated() = Authenticated;
+  const factory AuthState.unauthenticated() = Unauthenticated;
+  const factory AuthState.verified() = Verified;
+  const factory AuthState.unverified() = Unverified;
+  const factory AuthState.awaitingVerified() = AwaitingVerified;
+  const factory AuthState.donorFormCompleted() = DonorFormCompleted;
+  const factory AuthState.donorFormNotCompleted() = DonorFormNotCompleted;
 }
