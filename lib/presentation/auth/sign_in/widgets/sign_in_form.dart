3de95086081aec8dae21c2520f425a1193b7e475
David Michael II <knowdavidmichael@gmail.com>
2022-07-04 22:42:54 +0100

    refactor: Get.closeAllSnackbars() in sign in

--- lib/presentation/auth/sign_in/widgets/sign_in_form.dart
+++ lib/presentation/auth/sign_in/widgets/sign_in_form.dart
@@ -35,7 +35,7 @@ class SignInForm extends StatelessWidget {
               );
             },
             (_) {
-              Get.closeAllSnackbars();
+              JSnackbars.closeAll;
               authBloc.add(const AuthEvent.authCheckRequested());
               authBloc.add(const AuthEvent.authCheckVerified());
               if (authBloc.state is Unverified) {
