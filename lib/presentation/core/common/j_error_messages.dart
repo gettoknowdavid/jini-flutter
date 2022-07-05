2f8876b6aaa90acea3fe08915ffca3d0392891f6
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 18:59:22 +0100

    Create j_error_messages.dart

---
+++ lib/presentation/core/common/j_error_messages.dart
@@ -0,0 +1,7 @@
+class JErrorMessages {
+  JErrorMessages._();
+
+  static const String emailInUse =
+      'Seems like the email you provided is already in use.';
+  static const String serverError = 'Seems there\'s a server error.';
+}
