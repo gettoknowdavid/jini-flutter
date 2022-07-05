f95d940fade7d19ce2be0b72c357ed718a161403
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 02:59:15 +0100

    refactor: BottomNavButton

--- lib/presentation/core/layout/widgets/bottom_nav.dart
+++ lib/presentation/core/layout/widgets/bottom_nav.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jini/application/core/bottom_nav/bottom_nav_cubit.dart';
+import 'package:jini/presentation/core/layout/widgets/bottom_nav_button.dart';
 import 'package:jini/presentation/core/layout/widgets/request_button.dart';
 
 class BottomNav extends StatelessWidget {
@@ -38,41 +38,6 @@ class BottomNav extends StatelessWidget {
           ),
         );
       },
-    );
-  }
-}
-
-class BottomNavButton extends StatelessWidget {
-  const BottomNavButton({Key? key, required this.item}) : super(key: key);
-
-  final BottomNavItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    final bloc = BlocProvider.of<BottomNavCubit>(context);
-    final index = bloc.items.indexOf(item);
-
-    return Stack(
-      clipBehavior: Clip.none,
-      alignment: Alignment.center,
-      children: [
-        Icon(item.icon),
-        Positioned(
-          bottom: -14.h,
-          child: AnimatedOpacity(
-            duration: const Duration(milliseconds: 350),
-            opacity: bloc.currentIndex == index ? 1.0 : 0.0,
-            child: Container(
-              height: 3.h,
-              width: 30.sp,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.vertical(top: Radius.circular(19.r)),
-                color: Colors.pinkAccent,
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
