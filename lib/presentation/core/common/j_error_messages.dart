1a34b42aa2a51f28909af00945baa1df643202ed
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 19:06:06 +0100

    Update j_error_messages.dart

--- lib/presentation/core/common/j_error_messages.dart
+++ lib/presentation/core/common/j_error_messages.dart
@@ -4,4 +4,8 @@ class JErrorMessages {
   static const String emailInUse =
       'Seems like the email you provided is already in use.';
   static const String serverError = 'Seems there\'s a server error.';
+  static const String invalidEmailOrPassword =
+      'Oops! Looks like an invalid email or password.';
+  static const String invalidEmail = 'Oops! Invalid email';
+  static const String invalidPassword = 'Oops! Invalid password';
 }
