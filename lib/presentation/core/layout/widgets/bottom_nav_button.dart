a5ea7fdb7df61215530a54a3885f2318f0b23d4d
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 02:59:00 +0100

    feat: create bottom_nav_button.dart

---
+++ lib/presentation/core/layout/widgets/bottom_nav_button.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jini/application/core/bottom_nav/bottom_nav_cubit.dart';
+
+class BottomNavButton extends StatelessWidget {
+  const BottomNavButton({Key? key, required this.item}) : super(key: key);
+
+  final BottomNavItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<BottomNavCubit>(context);
+    final index = bloc.items.indexOf(item);
+
+    return Stack(
+      clipBehavior: Clip.none,
+      alignment: Alignment.center,
+      children: [
+        Icon(item.icon),
+        Positioned(
+          bottom: -14.h,
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 350),
+            opacity: bloc.currentIndex == index ? 1.0 : 0.0,
+            child: Container(
+              height: 3.h,
+              width: 30.sp,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(19.r)),
+                color: Colors.pinkAccent,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
