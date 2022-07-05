bcf14ee3ad3aeb54a7d88ad443173f673cce6817
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 18:54:49 +0100

    Create j_screen_util.dart

---
+++ lib/presentation/core/common/j_screen_util.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class JScreenUtil {
+  JScreenUtil._();
+
+  static final GLOBAL_PADDING = const EdgeInsets.symmetric(horizontal: 18).r;
+
+  static vSpace(num height) => ScreenUtil().setVerticalSpacing(height);
+}
