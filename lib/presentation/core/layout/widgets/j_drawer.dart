023097157c187f7e591a17cff954b255d92c36d8
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 11:48:10 +0100

    feat: create j_drawer widget

---
+++ lib/presentation/core/layout/widgets/j_drawer.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class JDrawer extends StatelessWidget {
+  const JDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer();
+  }
+}
