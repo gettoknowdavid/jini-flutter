e1101541f969961cd411c891226ae914d733fbba
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 18:05:45 +0100

    feat: update theme

--- lib/presentation/core/common/j_theme.dart
+++ lib/presentation/core/common/j_theme.dart
@@ -6,22 +6,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class JTheme {
   JTheme._();
 
+  static final Color _white = Color(0xFFFFFFFF);
   static final Color _primary = Color(0xFFF25774);
   static final Color _primaryDark = Color(0xFFF25774);
   static final Color _backgroundDark = Color(0xFF090911);
   static final Color _backgroundLight = Color(0xFFECEAEA);
-  static final swatch = MaterialColor(0xFFF25774, {
-    50: const Color.fromRGBO(250, 99, 147, .1),
-    100: const Color.fromRGBO(250, 99, 147, .2),
-    200: const Color.fromRGBO(250, 99, 147, .3),
-    300: const Color.fromRGBO(250, 99, 147, .4),
-    400: const Color.fromRGBO(250, 99, 147, .5),
-    500: const Color.fromRGBO(250, 99, 147, .6),
-    600: const Color.fromRGBO(250, 99, 147, .7),
-    700: const Color.fromRGBO(250, 99, 147, .8),
-    800: const Color.fromRGBO(250, 99, 147, .9),
-    900: const Color.fromRGBO(250, 99, 147, 1),
-  });
 
   static final _appBarTheme = AppBarTheme(
     backgroundColor: Colors.transparent,
@@ -51,11 +40,11 @@ class JTheme {
   static final _elevatedButtonTheme = ElevatedButtonThemeData(
     style: ElevatedButton.styleFrom(
       elevation: 16,
-      shadowColor: swatch.shade700,
+      shadowColor: _primary.withOpacity(0.7),
       primary: _primary,
       padding: EdgeInsets.all(20).r,
       textStyle: GoogleFonts.spaceGrotesk(
-        color: Colors.white,
+        color: _white,
         fontSize: 18.sp,
         fontWeight: FontWeight.w600,
       ),
@@ -71,11 +60,11 @@ class JTheme {
     return _baseLight.copyWith(
       colorScheme: ColorScheme.light().copyWith(
         primary: _primary,
-        onPrimary: Colors.white,
+        onPrimary: _white,
         secondary: _primary,
-        onSecondary: Colors.white,
-        error: swatch.shade900,
-        onError: Colors.white,
+        onSecondary: _white,
+        background: _backgroundLight,
+        surface: _backgroundLight,
       ),
       primaryColor: _primary,
       brightness: Brightness.light,
@@ -83,15 +72,18 @@ class JTheme {
       canvasColor: _backgroundLight,
       backgroundColor: _backgroundLight,
       scaffoldBackgroundColor: _backgroundLight,
-      progressIndicatorTheme: ProgressIndicatorThemeData(
-        color: _backgroundLight,
-      ),
       indicatorColor: _primary,
       appBarTheme: _appBarTheme,
       textButtonTheme: _textButtonTheme,
       drawerTheme: _drawerTheme,
       elevatedButtonTheme: _elevatedButtonTheme,
       pageTransitionsTheme: _pageTransitionsTheme,
+      progressIndicatorTheme: ProgressIndicatorThemeData(
+        color: _backgroundLight,
+      ),
+      textTheme: _baseLight.textTheme
+          .copyWith()
+          .apply(fontFamily: GoogleFonts.spaceGrotesk().fontFamily),
     );
   }
 
@@ -100,15 +92,11 @@ class JTheme {
     return _baseDark.copyWith(
       colorScheme: ColorScheme.dark().copyWith(
         primary: _primary,
-        onPrimary: Colors.white,
+        onPrimary: _white,
         secondary: _primary,
-        onSecondary: Colors.white,
+        onSecondary: _white,
         background: _backgroundDark,
-        onBackground: Colors.white,
-        error: swatch.shade900,
-        onError: Colors.white,
         surface: _backgroundLight,
-        onSurface: Colors.white,
       ),
       brightness: Brightness.dark,
       primaryColor: _primary,
@@ -116,15 +104,18 @@ class JTheme {
       canvasColor: _backgroundDark,
       backgroundColor: _backgroundDark,
       scaffoldBackgroundColor: _backgroundDark,
-      progressIndicatorTheme: ProgressIndicatorThemeData(
-        color: _backgroundDark,
-      ),
       indicatorColor: _primary,
       appBarTheme: _appBarTheme,
       textButtonTheme: _textButtonTheme,
       drawerTheme: _drawerTheme,
       elevatedButtonTheme: _elevatedButtonTheme,
       pageTransitionsTheme: _pageTransitionsTheme,
+      progressIndicatorTheme: ProgressIndicatorThemeData(
+        color: _backgroundDark,
+      ),
+      textTheme: _baseDark.textTheme
+          .copyWith()
+          .apply(fontFamily: GoogleFonts.spaceGrotesk().fontFamily),
     );
   }
 }
