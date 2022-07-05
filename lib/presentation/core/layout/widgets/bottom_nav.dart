a2c0bc13ad7b02cad2ad027a719bd4117eb8029c
David Michael II <knowdavidmichael@gmail.com>
2022-07-05 02:57:42 +0100

    refactor: request button in bottom_nav

--- lib/presentation/core/layout/widgets/bottom_nav.dart
+++ lib/presentation/core/layout/widgets/bottom_nav.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:jini/application/core/bottom_nav/bottom_nav_cubit.dart';
+import 'package:jini/presentation/core/layout/widgets/request_button.dart';
 
 class BottomNav extends StatelessWidget {
   const BottomNav({Key? key}) : super(key: key);
@@ -72,35 +73,6 @@ class BottomNavButton extends StatelessWidget {
           ),
         ),
       ],
-    );
-  }
-}
-
-class RequestButton extends StatelessWidget {
-  const RequestButton({Key? key, required this.item}) : super(key: key);
-
-  final BottomNavItem item;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Container(
-      height: 60.w,
-      width: 60.w,
-      child: Icon(item.icon, color: Colors.white),
-      decoration: BoxDecoration(
-        color: theme.primaryColor,
-        borderRadius: BorderRadius.circular(20).r,
-        boxShadow: [
-          BoxShadow(
-            color: theme.primaryColor.withOpacity(0.25),
-            spreadRadius: 1,
-            blurRadius: 12,
-            offset: Offset(0.w, 5.h),
-          ),
-        ],
-      ),
     );
   }
 }
