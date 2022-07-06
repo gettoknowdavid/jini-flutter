3eab5f899a60c29b6220660897871db2639d0175
David Michael II <knowdavidmichael@gmail.com>
2022-07-06 01:01:50 +0100

    Update j_screen_util.dart

--- lib/presentation/core/common/j_screen_util.dart
+++ lib/presentation/core/common/j_screen_util.dart
@@ -4,7 +4,14 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class JScreenUtil {
   JScreenUtil._();
 
-  static final GLOBAL_PADDING = const EdgeInsets.symmetric(horizontal: 18).r;
+  static final GLOBAL_PADDING = const EdgeInsets.symmetric(
+    horizontal: 18,
+    vertical: 20,
+  ).r;
+
+  static final BORDER_RADIUS = BorderRadius.circular(18).r;
+
+  static final CONTENT_PADDING = const EdgeInsets.fromLTRB(18, 22, 18, 22).r;
 
   static vSpace(num height) => ScreenUtil().setVerticalSpacing(height);
 
