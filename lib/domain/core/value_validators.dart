b9d10e73105e573ce33f2ac081a457b49ef6cae0
David Michael II <knowdavidmichael@gmail.com>
2022-07-06 02:58:31 +0100

    Update value_validators.dart

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -39,12 +39,16 @@ Either<ValueFailure<Gender>, Gender> validateGenderNotNull(Gender? input) {
 }
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
-  const emailRegex =
-      r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
-  if (RegExp(emailRegex).hasMatch(input)) {
-    return right(input);
+  if (input.isEmpty) {
+    return left(ValueFailure.empty(failedValue: input));
   } else {
-    return left(ValueFailure.invalidEmail(failedValue: input));
+    const emailRegex =
+        r"""^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+""";
+    if (RegExp(emailRegex).hasMatch(input)) {
+      return right(input);
+    } else {
+      return left(ValueFailure.invalidEmail(failedValue: input));
+    }
   }
 }
 
@@ -61,12 +65,16 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
   // Must contain at least 8 characters
   // Must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
   // Must contain a special characters
-  const pRegEx =
-      r"""^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%*?&]).{8,}$""";
-  if (RegExp(pRegEx).hasMatch(input)) {
-    return right(input);
+  if (input.isEmpty) {
+    return left(ValueFailure.empty(failedValue: input));
   } else {
-    return left(ValueFailure.invalidPassword(failedValue: input));
+    const pRegEx =
+        r"""^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[@$!%*?&]).{8,}$""";
+    if (RegExp(pRegEx).hasMatch(input)) {
+      return right(input);
+    } else {
+      return left(ValueFailure.invalidPassword(failedValue: input));
+    }
   }
 }
 
