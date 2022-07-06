615ba09007a0152488213ff72153c38e9fac7f9d
David Michael II <knowdavidmichael@gmail.com>
2022-07-06 01:01:47 +0100

    Update j_error_messages.dart

--- lib/presentation/core/common/j_error_messages.dart
+++ lib/presentation/core/common/j_error_messages.dart
@@ -6,6 +6,9 @@ class JErrorMessages {
   static const String serverError = 'Seems there\'s a server error.';
   static const String invalidEmailOrPassword =
       'Oops! Looks like an invalid email or password.';
+  static const String nameRequired = 'Your name is required';
   static const String invalidEmail = 'Oops! Invalid email';
-  static const String invalidPassword = 'Oops! Invalid password';
+  static const String invalidPassword =
+      'Password must contain: \n - Minimum 8 characters \n - At least 1 uppercase letter  \n - At least 1 digit  \n - At least 1 special character.';
+  static const String usrNotFound = 'Oops! No user with this email exists.';
 }
