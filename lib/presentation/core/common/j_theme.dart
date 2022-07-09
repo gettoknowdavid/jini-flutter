e1cabbddc33b3375f5432a028d28529a53da39a5
David Michael II <knowdavidmichael@gmail.com>
2022-07-09 02:10:44 +0100

    Update j_theme.dart

--- lib/presentation/core/common/j_theme.dart
+++ lib/presentation/core/common/j_theme.dart
@@ -1,4 +1,3 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -27,7 +26,6 @@ class JTheme {
 
   static final _pageTransitionsTheme = PageTransitionsTheme(
     builders: {
-      TargetPlatform.iOS: NoShadowCupertinoPageTransitionsBuilder(),
       TargetPlatform.android: ZoomPageTransitionsBuilder(),
     },
   );
