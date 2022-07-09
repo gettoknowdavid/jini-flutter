78a616c9e5640533250ff552954901bf78f53a19
David Michael II <knowdavidmichael@gmail.com>
2022-07-10 00:44:27 +0100

    feat: add theme switch to app layout

--- lib/presentation/core/layout/j_layout.dart
+++ lib/presentation/core/layout/j_layout.dart
@@ -6,6 +6,7 @@ import 'package:jini/application/core/bottom_nav/bottom_nav_cubit.dart';
 import 'package:jini/presentation/core/layout/widgets/bottom_nav.dart';
 import 'package:jini/presentation/core/layout/widgets/j_drawer.dart';
 import 'package:jini/presentation/core/routes/j_router.dart';
+import 'package:jini/presentation/core/widgets/j_theme_switch.dart';
 
 class JLayout extends StatelessWidget {
   @override
@@ -22,7 +23,12 @@ class JLayout extends StatelessWidget {
       child: BlocBuilder<BottomNavCubit, BottomNavState>(
         builder: (context, state) {
           return Scaffold(
-            appBar: AppBar(title: Text(state.title)),
+            appBar: AppBar(
+              title: Text(state.title),
+              actions: [
+                const JThemeSwitch(),
+              ],
+            ),
             drawer: JDrawer(),
             body: state.body,
             bottomNavigationBar: BottomNav(),
