08e8f5e172fc2eb04439a3b4b65360aea974d4de
David Michael II <knowdavidmichael@gmail.com>
2022-07-09 02:09:11 +0100

    Update j_layout.dart

--- lib/presentation/core/layout/j_layout.dart
+++ lib/presentation/core/layout/j_layout.dart
@@ -1,32 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:jini/application/auth/auth_bloc.dart';
 import 'package:jini/application/core/bottom_nav/bottom_nav_cubit.dart';
 import 'package:jini/presentation/core/layout/widgets/bottom_nav.dart';
 import 'package:jini/presentation/core/layout/widgets/j_drawer.dart';
-import 'package:jini/presentation/routes/j_router.dart';
+import 'package:jini/presentation/core/routes/j_router.dart';
 
-class JLayout extends StatefulWidget {
-  const JLayout({Key? key}) : super(key: key);
-
-  @override
-  State<JLayout> createState() => _JLayoutState();
-}
-
-class _JLayoutState extends State<JLayout> {
-  void initialization() async {
-    await Future.delayed(const Duration(seconds: 3));
-    FlutterNativeSplash.remove();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    initialization();
-  }
-
+class JLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocListener<AuthBloc, AuthState>(
