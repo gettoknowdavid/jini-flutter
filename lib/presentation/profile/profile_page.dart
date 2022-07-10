30178e10c229b52a586ab20e55a819e0d788a4e1
David Michael II <knowdavidmichael@gmail.com>
2022-07-10 20:02:47 +0100

    Update profile_page.dart

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -1,29 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jini/application/auth/auth_bloc.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key, this.title = 'Jini App'}) : super(key: key);
-  final String title;
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final bloc = BlocProvider.of<AuthBloc>(context);
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              title.toUpperCase(),
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            ElevatedButton(
-              onPressed: () => bloc.add(AuthEvent.authSignedOut()),
-              child: Text('Sign Out'),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Profile'),
       ),
     );
   }
