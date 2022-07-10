972cc0778155d129c7b6d4a3ef37376eb69fba93
David Michael II <knowdavidmichael@gmail.com>
2022-07-10 20:02:35 +0100

    Update bottom_nav_cubit.dart

--- lib/application/core/bottom_nav/bottom_nav_cubit.dart
+++ lib/application/core/bottom_nav/bottom_nav_cubit.dart
@@ -2,7 +2,10 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
+import 'package:jini/presentation/event/event_page.dart';
+import 'package:jini/presentation/home/home_page.dart';
 import 'package:jini/presentation/profile/profile_page.dart';
+import 'package:jini/presentation/request/create_request_page.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 part 'bottom_nav_state.dart';
@@ -20,8 +23,8 @@ class BottomNavCubit extends Cubit<BottomNavState> {
       case 0:
         emit(
           BottomNavState(
-            title: 'Jini App',
-            body: const ProfilePage(title: 'Jini App'),
+            title: 'Home',
+            body: const HomePage(),
           ),
         );
         break;
@@ -29,7 +32,7 @@ class BottomNavCubit extends Cubit<BottomNavState> {
         emit(
           BottomNavState(
             title: 'Make Request',
-            body: const ProfilePage(title: 'Make Request'),
+            body: const CreateRequestPage(),
           ),
         );
         break;
@@ -37,7 +40,7 @@ class BottomNavCubit extends Cubit<BottomNavState> {
         emit(
           BottomNavState(
             title: 'Events',
-            body: const ProfilePage(title: 'Events'),
+            body: const EventPage(),
           ),
         );
         break;
@@ -45,15 +48,15 @@ class BottomNavCubit extends Cubit<BottomNavState> {
         emit(
           BottomNavState(
             title: 'Home',
-            body: const ProfilePage(title: 'Home'),
+            body: const HomePage(),
           ),
         );
     }
   }
 
   List<BottomNavItem> items = [
-    BottomNavItem(title: 'Jini App', icon: PhosphorIcons.houseSimpleBold),
-    BottomNavItem(title: 'Make Request', icon: PhosphorIcons.dropBold),
+    BottomNavItem(title: 'Home', icon: PhosphorIcons.houseSimpleBold),
+    BottomNavItem(title: 'Create Request', icon: PhosphorIcons.dropBold),
     BottomNavItem(title: 'Events', icon: PhosphorIcons.calendarBold),
   ];
 }
