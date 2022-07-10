93a70661bb2fabedd37fd6f2f29037c054fd21ac
David Michael II <knowdavidmichael@gmail.com>
2022-07-10 20:02:49 +0100

    Create create_request_page.dart

---
+++ lib/presentation/request/create_request_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateRequestPage extends StatelessWidget {
+  const CreateRequestPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Create Request'),
+      ),
+    );
+  }
+}
