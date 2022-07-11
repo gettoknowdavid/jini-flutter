9b41714eceab2210338d85d9e84302f198db2772
David Michael II <knowdavidmichael@gmail.com>
2022-07-11 20:21:03 +0100

    feat: add divider and r singletons to j_screen_util

--- lib/presentation/core/common/j_screen_util.dart
+++ lib/presentation/core/common/j_screen_util.dart
@@ -4,6 +4,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class JScreenUtil {
   JScreenUtil._();
 
+  static final divider = Divider(height: 1.r, indent: 18.r, endIndent: 18.r);
+
   static final globalPadding = const EdgeInsets.symmetric(
     horizontal: 18,
     vertical: 20,
@@ -18,6 +20,10 @@ class JScreenUtil {
   static final contentRadius = const EdgeInsets.fromLTRB(18, 22, 18, 22).r;
 
   static fontSize(num value) => value.r;
+
+  static radius(num value) => value.r;
+
+  static r(num value) => value.r;
 
   static vSpace(num height) => ScreenUtil().setVerticalSpacing(height);
 
