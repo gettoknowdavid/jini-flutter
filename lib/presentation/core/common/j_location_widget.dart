28212248a648437d2afa5c5ceece68e650ceaf29
David Michael II <knowdavidmichael@gmail.com>
2022-07-12 16:41:56 +0100

    Update j_location_widget.dart

--- lib/presentation/core/common/j_location_widget.dart
+++ lib/presentation/core/common/j_location_widget.dart
@@ -1,6 +1,9 @@
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jini/application/profile/profile_bloc.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
+import 'package:jini/presentation/profile/widgets/pencil_button.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class JLocationWidget extends StatelessWidget {
@@ -8,28 +11,43 @@ class JLocationWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Parent(
-      gesture: Gestures()..onTap(() {}),
-      style: ParentStyle()
-        ..padding(vertical: JScreenUtil.r(6))
-        ..ripple(true),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            PhosphorIcons.navigationArrow,
-            color: Colors.white,
-            size: JScreenUtil.fontSize(18),
-          ),
-          JScreenUtil.hSpace(6),
-          Txt(
-            'Port Harcourt, Nigeria',
-            style: TxtStyle()
-              ..textColor(Colors.white70)
-              ..fontSize(JScreenUtil.fontSize(16)),
-          ),
-        ],
-      ),
+    final bloc = BlocProvider.of<ProfileBloc>(context);
+    bool isEditing = bloc.state.isEditing;
+
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      listenWhen: (p, c) => p.isEditing != c.isEditing,
+      listener: (context, state) => isEditing = state.isEditing,
+      builder: (context, state) {
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Parent(
+              gesture: Gestures()..onTap(() {}),
+              style: ParentStyle()
+                ..padding(vertical: JScreenUtil.r(6))
+                ..ripple(true),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    PhosphorIcons.navigationArrow,
+                    color: Colors.white,
+                    size: JScreenUtil.fontSize(18),
+                  ),
+                  JScreenUtil.hSpace(6),
+                  Txt(
+                    'Port Harcourt, Nigeria',
+                    style: TxtStyle()
+                      ..textColor(Colors.white70)
+                      ..fontSize(JScreenUtil.fontSize(16)),
+                  ),
+                ],
+              ),
+            ),
+            if (isEditing) PencilButton(onTap: () {}, size: 18),
+          ],
+        );
+      },
     );
   }
 }
