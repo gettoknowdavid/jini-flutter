b735c7d7f7800a3015007d24ad2ecb318a9e83bd
David Michael II <knowdavidmichael@gmail.com>
2022-07-12 02:48:12 +0100

    Update edit_profile_page.dart

---
+++ lib/presentation/profile/edit_profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Profile'),
+      ),
+    );
+  }
+}
