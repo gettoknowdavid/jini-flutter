00962494699e916c5306216550bf0ff3a060b45b
David Michael II <knowdavidmichael@gmail.com>
2022-07-12 17:01:18 +0100

    feat: add IName to profile_state

--- lib/application/profile/profile_state.dart
+++ lib/application/profile/profile_state.dart
@@ -2,7 +2,17 @@ part of 'profile_bloc.dart';
 
 @freezed
 class ProfileState with _$ProfileState {
-  const factory ProfileState({required bool isEditing}) = _ProfileState;
+  const factory ProfileState({
+    required bool isEditing,
+    required IName name,
+  }) = _ProfileState;
 
-  factory ProfileState.initial() => ProfileState(isEditing: false);
+  factory ProfileState.initial() => ProfileState(
+        isEditing: false,
+        name: IName(''),
+      );
+  factory ProfileState.editing() => ProfileState(
+        isEditing: true,
+        name: IName(''),
+      );
 }
