7e15b96072ab32f0a42a8adbd49eb030a5c713b6
David Michael II <knowdavidmichael@gmail.com>
2022-07-12 16:42:11 +0100

    Update profile_details_item.dart

--- lib/presentation/profile/widgets/profile_details_item.dart
+++ lib/presentation/profile/widgets/profile_details_item.dart
@@ -1,6 +1,9 @@
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jini/application/profile/profile_bloc.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
+import 'package:jini/presentation/profile/widgets/pencil_button.dart';
 
 class ProfileDetailsItem extends StatelessWidget {
   const ProfileDetailsItem({
@@ -17,27 +20,38 @@ class ProfileDetailsItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final bloc = BlocProvider.of<ProfileBloc>(context);
+    bool isEditing = bloc.state.isEditing;
 
-    return Parent(
-      gesture: Gestures()..onTap(() {}),
-      style: ParentStyle()
-        ..padding(all: JScreenUtil.r(18))
-        ..ripple(true),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Icon(icon, color: theme.primaryColor),
-          JScreenUtil.hSpace(20),
-          Column(
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      bloc: bloc,
+      listenWhen: (p, c) => p.isEditing != c.isEditing,
+      listener: (context, state) => isEditing = state.isEditing,
+      builder: (context, state) {
+        return Parent(
+          gesture: Gestures()..onTap(() {}),
+          style: ParentStyle()
+            ..padding(all: JScreenUtil.r(18))
+            ..ripple(true),
+          child: Row(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Txt(title, style: TxtStyle()..fontSize(12)),
-              JScreenUtil.vSpace(4),
-              Txt(value, style: TxtStyle()..fontSize(16)),
+              Icon(icon, color: theme.primaryColor),
+              JScreenUtil.hSpace(20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Txt(title, style: TxtStyle()..fontSize(12)),
+                  JScreenUtil.vSpace(4),
+                  Txt(value, style: TxtStyle()..fontSize(16)),
+                ],
+              ),
+              const Spacer(),
+              if (isEditing) PencilButton(onTap: () {}, color: Colors.black)
             ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
