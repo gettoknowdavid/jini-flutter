1c984d0bcec7c40b45d64a92dfca1f42f37c0635
David Michael II <knowdavidmichael@gmail.com>
2022-07-12 16:41:49 +0100

    Update profile_event.dart

--- lib/application/profile/profile_event.dart
+++ lib/application/profile/profile_event.dart
@@ -3,4 +3,5 @@ part of 'profile_bloc.dart';
 @freezed
 class ProfileEvent with _$ProfileEvent {
   const factory ProfileEvent.started() = _Started;
+  const factory ProfileEvent.editPressed(bool value) = _ProfileEditPressed;
 }
