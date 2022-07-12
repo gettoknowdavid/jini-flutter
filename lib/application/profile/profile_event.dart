dac83476be6740a6ddd77980baa8390a2a8525ef
David Michael II <knowdavidmichael@gmail.com>
2022-07-12 17:01:39 +0100

    feat: add ProfileUpdated event to profile_event

--- lib/application/profile/profile_event.dart
+++ lib/application/profile/profile_event.dart
@@ -4,4 +4,5 @@ part of 'profile_bloc.dart';
 class ProfileEvent with _$ProfileEvent {
   const factory ProfileEvent.started() = _Started;
   const factory ProfileEvent.editPressed(bool value) = _ProfileEditPressed;
+  const factory ProfileEvent.profileUpdated() = _ProfileUpdated;
 }
