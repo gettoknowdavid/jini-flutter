07b36d94c0fe83d3cb8d221b6d5cf2c7886e272a
David Michael II <knowdavidmichael@gmail.com>
2022-07-14 23:56:22 +0100

    Update profile_page.dart

--- lib/presentation/profile/profile_page.dart
+++ lib/presentation/profile/profile_page.dart
@@ -1,29 +1,42 @@
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+import 'package:jini/application/profile/profile_bloc.dart';
+import 'package:jini/domain/core/gender.dart';
+import 'package:jini/infrastructure/auth/j_user_dtos.dart';
 import 'package:jini/presentation/core/common/j_avatar.dart';
 import 'package:jini/presentation/core/common/j_location_widget.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/core/common/j_widget_styles.dart';
 import 'package:jini/presentation/core/widgets/j_back_button.dart';
+import 'package:jini/presentation/profile/widgets/edit_bottom_sheet.dart';
 import 'package:jini/presentation/profile/widgets/profile_blood_group_widget.dart';
 import 'package:jini/presentation/profile/widgets/profile_details_item.dart';
+import 'package:jini/presentation/profile/widgets/profile_email_widget.dart';
 import 'package:jini/presentation/profile/widgets/profile_save_button.dart';
 import 'package:jini/presentation/profile/widgets/profile_stats.dart';
 import 'package:jini/presentation/profile/widgets/profile_user_name.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:jini/domain/core/user_type.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // final bloc = BlocProvider.of<ProfileBloc>(context);
-    // bool isEditing = false;
+    final bloc = BlocProvider.of<ProfileBloc>(context);
 
     return Scaffold(
       extendBodyBehindAppBar: true,
       appBar: AppBar(
-        leading: const JBackButton(color: Colors.white),
+        leading: JBackButton(
+          color: Colors.white,
+          onTap: () {
+            bloc.add(ProfileEvent.editPressed(false));
+            Get.back();
+          },
+        ),
         actions: [const ProfileSaveButton()],
       ),
       body: SingleChildScrollView(
@@ -34,9 +47,11 @@ class ProfilePage extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const JAvatar(showBadge: true),
+                  const JAvatar(),
                   JScreenUtil.vSpace(30),
                   const ProfileUserName(),
+                  JScreenUtil.vSpace(4),
+                  const ProfileEmailWidget(),
                   JScreenUtil.vSpace(4),
                   const JLocationWidget(),
                   JScreenUtil.vSpace(30),
@@ -47,45 +62,100 @@ class ProfilePage extends StatelessWidget {
             JScreenUtil.vSpace(30),
             const ProfileBloodGroupWidget(),
             JScreenUtil.vSpace(30),
-            const ProfileDetailsItem(
-              title: 'Email',
-              value: 'knowdavidmichael@gmail.com',
-              icon: PhosphorIcons.envelope,
-            ),
-            JScreenUtil.vSpace(10),
-            const ProfileDetailsItem(
-              title: 'Phone',
-              value: '+234 816 028 5225',
-              icon: PhosphorIcons.phone,
-            ),
-            JScreenUtil.vSpace(10),
-            const ProfileDetailsItem(
-              title: 'Gender',
-              value: 'Male',
-              icon: PhosphorIcons.genderIntersex,
-            ),
-            JScreenUtil.divider,
-            const ProfileDetailsItem(
-              title: 'Age',
-              value: '24',
-              icon: PhosphorIcons.calendarBlank,
-            ),
-            JScreenUtil.divider,
-            const ProfileDetailsItem(
-              title: 'Height',
-              value: '6.2',
-              icon: PhosphorIcons.arrowsVertical,
-            ),
-            JScreenUtil.divider,
-            const ProfileDetailsItem(
-              title: 'Weight',
-              value: '65.9',
-              icon: PhosphorIcons.arrowsHorizontal,
-            ),
-            JScreenUtil.vSpace(30)
+            const _OtherDetails(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _OtherDetails extends StatelessWidget {
+  const _OtherDetails({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<ProfileBloc>(context);
+
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      bloc: bloc,
+      listener: (context, state) {},
+      builder: (context, state) {
+        final user = JUserDto.fromDomain(bloc.state.user);
+
+        return ListView(
+          primary: false,
+          shrinkWrap: true,
+          padding: EdgeInsets.zero,
+          children: [
+            Padding(
+              padding: JScreenUtil.padFromLTRB(18, 0, 18, 0),
+              child: const Text('Other Details'),
+            ),
+            JScreenUtil.vSpace(10),
+            ProfileDetailsItem(
+              title: 'Account Type',
+              icon: PhosphorIcons.user,
+              value: user.userType!.value,
+              onTap: () => Get.bottomSheet(
+                const EditUserTypeBottomSheet(),
+                isDismissible: false,
+              ),
+            ),
+            JScreenUtil.vSpace(10),
+            ProfileDetailsItem(
+              title: 'Phone',
+              icon: PhosphorIcons.phone,
+              value: user.phone,
+              onTap: () => Get.bottomSheet(
+                const EditPhoneBottomSheet(),
+                isDismissible: false,
+              ),
+            ),
+            JScreenUtil.vSpace(10),
+            ProfileDetailsItem(
+              title: 'Gender',
+              icon: PhosphorIcons.genderIntersex,
+              value: user.gender?.value,
+              onTap: () => Get.bottomSheet(
+                const EditGenderBottomSheet(),
+                isDismissible: false,
+              ),
+            ),
+            JScreenUtil.divider,
+            ProfileDetailsItem(
+              title: 'Age',
+              icon: PhosphorIcons.calendarBlank,
+              value: user.age == 0 ? null : user.age?.toString(),
+              onTap: () => Get.bottomSheet(
+                const EditAgeBottomSheet(),
+                isDismissible: false,
+              ),
+            ),
+            JScreenUtil.divider,
+            ProfileDetailsItem(
+              title: 'Height',
+              icon: PhosphorIcons.arrowsVertical,
+              value: user.height == 0 ? null : user.height?.toString(),
+              onTap: () => Get.bottomSheet(
+                const EditHeightBottomSheet(),
+                isDismissible: false,
+              ),
+            ),
+            JScreenUtil.divider,
+            ProfileDetailsItem(
+              title: 'Weight',
+              icon: PhosphorIcons.arrowsHorizontal,
+              value: user.weight == null ? null : user.weight?.toString(),
+              onTap: () => Get.bottomSheet(
+                const EditWeightBottomSheet(),
+                isDismissible: false,
+              ),
+            ),
+            JScreenUtil.vSpace(30)
+          ],
+        );
+      },
     );
   }
 }
