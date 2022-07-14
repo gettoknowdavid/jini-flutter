de56feadd3ca5ed56bfeba8450473dcae83dc997
David Michael II <knowdavidmichael@gmail.com>
2022-07-14 23:55:15 +0100

    Update profile_event.dart

--- lib/application/profile/profile_event.dart
+++ lib/application/profile/profile_event.dart
@@ -2,7 +2,21 @@ part of 'profile_bloc.dart';
 
 @freezed
 class ProfileEvent with _$ProfileEvent {
-  const factory ProfileEvent.started() = _Started;
+  const factory ProfileEvent.initialized() = _Initialized;
+  const factory ProfileEvent.nameChanged(String name) = _NameChanged;
+  const factory ProfileEvent.emailChanged(String email) = _EmailChanged;
+  const factory ProfileEvent.phoneChanged(String phone) = _PhoneChanged;
+  const factory ProfileEvent.genderChanged(Gender gender) = _GenderChanged;
+  const factory ProfileEvent.bloodGroupChanged(BloodGroup bloodGroup) =
+      _BloodGroupChanged;
+  const factory ProfileEvent.userTypeChanged(UserType userType) =
+      _UserTypeChanged;
+  const factory ProfileEvent.ageChanged(num age, UserType userType) =
+      _AgeChanged;
+  const factory ProfileEvent.heightChanged(num height) = _HeightChanged;
+  const factory ProfileEvent.weightChanged(num weight, UserType userType) =
+      _WeightChanged;
+
   const factory ProfileEvent.editPressed(bool value) = _ProfileEditPressed;
   const factory ProfileEvent.profileUpdated() = _ProfileUpdated;
 }
