4bfa09936924ce0f0f19c9807b45061dad8baaf9
David Michael II <knowdavidmichael@gmail.com>
2022-07-14 23:56:33 +0100

    Update profile_user_name.dart

--- lib/presentation/profile/widgets/profile_user_name.dart
+++ lib/presentation/profile/widgets/profile_user_name.dart
@@ -1,8 +1,11 @@
 import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:jini/application/profile/profile_bloc.dart';
+import 'package:jini/infrastructure/auth/j_user_dtos.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
+import 'package:jini/presentation/profile/widgets/edit_bottom_sheet.dart';
 import 'package:jini/presentation/profile/widgets/pencil_button.dart';
 
 class ProfileUserName extends StatelessWidget {
@@ -13,20 +16,26 @@ class ProfileUserName extends StatelessWidget {
     final bloc = BlocProvider.of<ProfileBloc>(context);
     bool isEditing = bloc.state.isEditing;
 
+    _onTap() => isEditing ? Get.bottomSheet(const EditNameBottomSheet()) : null;
+
     return BlocConsumer<ProfileBloc, ProfileState>(
+      bloc: bloc,
       listenWhen: (p, c) => p.isEditing != c.isEditing,
       listener: (context, state) => isEditing = state.isEditing,
       builder: (context, state) {
+        final user = JUserDto.fromDomain(bloc.state.user);
+
         return Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Txt(
-              'David Michael',
+              user.name,
+              gesture: Gestures()..onTap(_onTap),
               style: TxtStyle()
                 ..textColor(Colors.white)
                 ..fontSize(JScreenUtil.fontSize(32)),
             ),
-            if (isEditing) PencilButton(onTap: () {}),
+            if (isEditing) PencilButton(onTap: () {}, color: Colors.white),
           ],
         );
       },
