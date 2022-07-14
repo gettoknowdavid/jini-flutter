15673d92a703966b5a1d3ccf520296fba0167791
David Michael II <knowdavidmichael@gmail.com>
2022-07-14 23:56:02 +0100

    Update firebase_user_mapper.dart

--- lib/infrastructure/auth/firebase_user_mapper.dart
+++ lib/infrastructure/auth/firebase_user_mapper.dart
@@ -1,7 +1,6 @@
 import 'package:injectable/injectable.dart';
 import 'package:jini/domain/auth/j_user.dart';
 import 'package:jini/domain/auth/value_objects.dart';
-import 'package:jini/domain/core/gender.dart';
 import 'package:jini/domain/core/geo.dart';
 import 'package:jini/domain/core/value_object.dart';
 import 'package:jini/infrastructure/auth/j_user_dtos.dart';
@@ -15,23 +14,23 @@ class FirebaseUserMapper {
 
     return JUser(
       uid: Uid.fromUniqueString(user.uid),
-      age: user.age == null ? IAge(0) : IAge(user.age!),
-      bloodGroup: IBloodGroup(user.bloodGroup),
+      age: user.age == null ? null : IAge(user.age!, user.userType!),
+      bloodGroup: user.bloodGroup == null ? null : IBloodGroup(user.bloodGroup),
       city: user.city == null ? null : user.city,
       eligible: user.eligible == null ? null : user.eligible,
       email: IEmailAddress(user.email),
       formComplete: user.formComplete == null ? null : user.formComplete,
-      gender:
-          user.gender == null ? IGender(Gender.other) : IGender(user.gender!),
-      height: user.height == null ? IHeight(0) : IHeight(5.6),
+      gender: user.gender == null ? null : IGender(user.gender!),
+      height: user.height == null ? null : IHeight(user.height!),
       initEdit: user.initEdit == null ? null : user.initEdit,
       location: user.location == null
           ? null
           : Geo(user.location!.latitude, user.location!.latitude),
       name: IName(user.name),
-      phone: user.phone == null ? IPhone('01860285225') : IPhone(user.phone!),
+      phone: user.phone == null ? null : IPhone(user.phone!),
       userType: IUserType(user.userType),
-      weight: user.weight == null ? IWeight(0) : IWeight(user.weight!),
+      weight:
+          user.weight == null ? null : IWeight(user.weight!, user.userType!),
     );
   }
 }
