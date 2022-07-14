44a45de40b5c5070a31f1de7389cfe2018c32091
David Michael II <knowdavidmichael@gmail.com>
2022-07-14 23:56:30 +0100

    Update profile_details_item.dart

--- lib/presentation/profile/widgets/profile_details_item.dart
+++ lib/presentation/profile/widgets/profile_details_item.dart
@@ -9,17 +9,20 @@ class ProfileDetailsItem extends StatelessWidget {
   const ProfileDetailsItem({
     Key? key,
     required this.title,
-    required this.value,
+    this.value,
     required this.icon,
+    required this.onTap,
   }) : super(key: key);
 
   final String title;
-  final String value;
+  final String? value;
   final IconData icon;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
     final bloc = BlocProvider.of<ProfileBloc>(context);
     bool isEditing = bloc.state.isEditing;
 
@@ -29,7 +32,7 @@ class ProfileDetailsItem extends StatelessWidget {
       listener: (context, state) => isEditing = state.isEditing,
       builder: (context, state) {
         return Parent(
-          gesture: Gestures()..onTap(() {}),
+          gesture: Gestures()..onTap(onTap),
           style: ParentStyle()
             ..padding(all: JScreenUtil.r(18))
             ..ripple(true),
@@ -41,13 +44,16 @@ class ProfileDetailsItem extends StatelessWidget {
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Txt(title, style: TxtStyle()..fontSize(12)),
+                  Text(title, style: textTheme.caption),
                   JScreenUtil.vSpace(4),
-                  Txt(value, style: TxtStyle()..fontSize(16)),
+                  Text(
+                    value == null ? 'Add your $title' : value!,
+                    style: textTheme.titleMedium,
+                  ),
                 ],
               ),
               const Spacer(),
-              if (isEditing) PencilButton(onTap: () {}, color: Colors.black)
+              if (isEditing) PencilButton(onTap: () {})
             ],
           ),
         );
