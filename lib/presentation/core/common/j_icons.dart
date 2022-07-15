4408de82eaaa7655f98801f2bc719a38a0cda608
David Michael II <knowdavidmichael@gmail.com>
2022-07-15 20:52:20 +0100

    feat: create JIcons

---
+++ lib/presentation/core/common/j_icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:jini/presentation/core/common/j_screen_util.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
+
+class JIcons {
+  JIcons._();
+
+  static final pencil = Icon(
+    PhosphorIcons.pencil,
+    color: Colors.white,
+    size: JScreenUtil.r(16),
+  );
+}
