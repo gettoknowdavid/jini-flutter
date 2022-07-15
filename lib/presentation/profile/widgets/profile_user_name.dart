3032d25992a78c1d915e920a44c7b65cbd604fa5
David Michael II <knowdavidmichael@gmail.com>
2022-07-15 20:51:36 +0100

    feat: add edit icon to name

--- lib/presentation/profile/widgets/profile_user_name.dart
+++ lib/presentation/profile/widgets/profile_user_name.dart
@@ -1,4 +1,3 @@
-import 'package:division/division.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -6,7 +5,7 @@ import 'package:jini/application/profile/profile_bloc.dart';
 import 'package:jini/infrastructure/auth/j_user_dtos.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/profile/widgets/edit_bottom_sheet.dart';
-import 'package:jini/presentation/profile/widgets/pencil_button.dart';
+import 'package:jini/presentation/core/common/j_icons.dart';
 
 class ProfileUserName extends StatelessWidget {
   const ProfileUserName({Key? key}) : super(key: key);
@@ -16,27 +15,36 @@ class ProfileUserName extends StatelessWidget {
     final bloc = BlocProvider.of<ProfileBloc>(context);
     bool isEditing = bloc.state.isEditing;
 
-    _onTap() => isEditing ? Get.bottomSheet(const EditNameBottomSheet()) : null;
+    _onTap() => Get.bottomSheet(const EditNameBottomSheet());
 
     return BlocConsumer<ProfileBloc, ProfileState>(
       bloc: bloc,
-      listenWhen: (p, c) => p.isEditing != c.isEditing,
-      listener: (context, state) => isEditing = state.isEditing,
+      listenWhen: (p, c) =>
+          p.isSaving != c.isSaving || p.isEditing != c.isEditing,
+      listener: (context, state) => {
+        state.isSaving,
+        isEditing = state.isEditing,
+      },
       builder: (context, state) {
         final user = JUserDto.fromDomain(bloc.state.user);
 
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Txt(
-              user.name,
-              gesture: Gestures()..onTap(_onTap),
-              style: TxtStyle()
-                ..textColor(Colors.white)
-                ..fontSize(JScreenUtil.fontSize(32)),
-            ),
-            if (isEditing) PencilButton(onTap: () {}, color: Colors.white),
-          ],
+        return GestureDetector(
+          onTap: isEditing ? _onTap : null,
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                user.name,
+                style: Theme.of(context).textTheme.headline5?.copyWith(
+                      fontSize: JScreenUtil.fontSize(32),
+                      color: Colors.white,
+                    ),
+              ),
+              JScreenUtil.hSpace(4),
+              if (isEditing) JIcons.pencil,
+            ],
+          ),
         );
       },
     );
