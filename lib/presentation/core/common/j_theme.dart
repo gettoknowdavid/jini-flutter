5be918f5b0f9d8906f598bccb3300118c7dde0df
David Michael II <knowdavidmichael@gmail.com>
2022-07-17 16:42:12 +0100

    Update j_theme.dart

--- lib/presentation/core/common/j_theme.dart
+++ lib/presentation/core/common/j_theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 
 class JTheme {
