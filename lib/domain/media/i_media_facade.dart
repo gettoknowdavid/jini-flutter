c60000ba6ec9951a4be1b186f6bd55316720c6f5
David Michael II <knowdavidmichael@gmail.com>
2022-07-17 22:59:15 +0100

    feat: add upload method for firebase storage

--- lib/domain/media/i_media_facade.dart
+++ lib/domain/media/i_media_facade.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 abstract class IMediaFacade {
   Future<Option<File?>> getImage();
+
+  Future<void> upload(String path, String name, Reference reference);
 }
