bb0cf450f081983ab719c5646a1cdc49eeee9d15
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 00:19:56 +0100

    fix: verification_page

--- lib/presentation/auth/verification/verification_page.dart
+++ lib/presentation/auth/verification/verification_page.dart
@@ -3,14 +3,14 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:jini/application/auth/auth_bloc.dart';
 import 'package:jini/application/profile/profile_bloc.dart';
 import 'package:jini/di/injection.dart';
-import 'package:jini/application/auth/auth_bloc.dart';
 import 'package:jini/presentation/core/common/image_resources.dart';
+import 'package:jini/presentation/core/common/j_page.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
-import 'package:jini/presentation/core/widgets/j_background.dart';
-import 'package:jini/presentation/core/widgets/j_button.dart';
 import 'package:jini/presentation/core/routes/j_router.dart';
+import 'package:jini/presentation/core/widgets/j_button.dart';
 
 class VerificationPage extends StatefulWidget {
   const VerificationPage({Key? key}) : super(key: key);
@@ -21,17 +21,6 @@ class VerificationPage extends StatefulWidget {
 
 class _VerificationPageState extends State<VerificationPage> {
   Timer? timer;
-
-  @override
-  void initState() {
-    getIt<AuthBloc>().add(const AuthEvent.authCheckVerified());
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,57 +51,62 @@ class _VerificationPageState extends State<VerificationPage> {
         );
       },
       builder: (context, state) {
-        return Scaffold(
-          body: Stack(
-            alignment: Alignment.center,
-            children: [
-              const JBackground(),
-              Padding(
-                padding: JScreenUtil.globalPadding,
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+        return JPage(
+          child: Padding(
+            padding: JScreenUtil.globalPadding,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  JScreenUtil.vSpace(10),
+                  Image.asset(ImageResources.verifyEmail),
+                  JScreenUtil.vSpace(18),
+                  JButton(
+                    title: 'Open email app',
+                    onPressed: () => bloc.add(AuthEvent.openMailApp()),
+                  ),
+                  JScreenUtil.vSpace(20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      JScreenUtil.vSpace(10),
-                      Image.asset(ImageResources.verifyEmail),
-                      JScreenUtil.vSpace(18),
-                      JButton(
-                        title: 'Open email app',
-                        onPressed: () => bloc.add(AuthEvent.openMailApp()),
+                      TextButton(
+                        onPressed: () {
+                          bloc.add(AuthEvent.sendVerifiedEmail());
+                        },
+                        child: const Text('Resend Email'),
+                        style: TextButton.styleFrom(
+                          textStyle: textTheme.titleSmall,
+                        ),
                       ),
-                      JScreenUtil.vSpace(20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          TextButton(
-                            onPressed: () {
-                              bloc.add(AuthEvent.sendVerifiedEmail());
-                            },
-                            child: const Text('Resend Email'),
-                            style: TextButton.styleFrom(
-                              textStyle: textTheme.titleSmall,
-                            ),
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              bloc.add(AuthEvent.authSignedOut());
-                            },
-                            child: const Text('Cancel'),
-                            style: TextButton.styleFrom(
-                              textStyle: textTheme.titleSmall,
-                            ),
-                          ),
-                        ],
+                      TextButton(
+                        onPressed: () {
+                          bloc.add(AuthEvent.authSignedOut());
+                        },
+                        child: const Text('Cancel'),
+                        style: TextButton.styleFrom(
+                          textStyle: textTheme.titleSmall,
+                        ),
                       ),
                     ],
                   ),
-                ),
+                ],
               ),
-            ],
+            ),
           ),
         );
       },
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    getIt<AuthBloc>().add(const AuthEvent.authCheckVerified());
+    super.initState();
   }
 }
