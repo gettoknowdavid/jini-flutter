944e13c20671190552463643cab46bfb41b696fa
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 00:13:09 +0100

    feat: add appBar and extendBody bool to j_page

--- lib/presentation/core/common/j_page.dart
+++ lib/presentation/core/common/j_page.dart
@@ -1,38 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
+import 'package:jini/presentation/core/widgets/j_background.dart';
 
 class JPage extends StatelessWidget {
   const JPage({
     Key? key,
     required this.child,
     this.loading = false,
+    this.appBar,
+    this.extendBodyBehindAppBar = false,
   }) : super(key: key);
 
   final Widget child;
   final bool loading;
+  final AppBar? appBar;
+  final bool extendBodyBehindAppBar;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        child,
-        if (loading)
-          Align(
-            alignment: Alignment.center,
-            child: Container(
-              width: JScreenUtil.sw(1),
-              height: JScreenUtil.sh(1),
-              color: Colors.black54,
-              child: Center(
-                child: Container(
-                  width: JScreenUtil.sw(0.1),
-                  height: JScreenUtil.sw(0.1),
-                  child: const CircularProgressIndicator.adaptive(),
+    return Scaffold(
+      appBar: appBar,
+      extendBodyBehindAppBar: extendBodyBehindAppBar,
+      body: Stack(
+        children: [
+          const JBackground(),
+          child,
+          if (loading)
+            Align(
+              alignment: Alignment.center,
+              child: Container(
+                width: JScreenUtil.sw(1),
+                height: JScreenUtil.sh(1),
+                color: Colors.black54,
+                child: Center(
+                  child: Container(
+                    width: JScreenUtil.sw(0.1),
+                    height: JScreenUtil.sw(0.1),
+                    child: const CircularProgressIndicator.adaptive(),
+                  ),
                 ),
               ),
             ),
-          ),
-      ],
+        ],
+      ),
     );
   }
 }
