ab10c6e948d40b0489e7e5033516bc94b5ee1801
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 00:05:33 +0100

    Update sign_up_page.dart

--- lib/presentation/auth/sign_up/sign_up_page.dart
+++ lib/presentation/auth/sign_up/sign_up_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get.dart';
 import 'package:jini/application/auth/sign_up/sign_up_bloc.dart';
 import 'package:jini/presentation/auth/sign_up/widgets/sign_up_form.dart';
-import 'package:jini/presentation/core/common/j_error_messages.dart';
+import 'package:jini/presentation/core/common/j_page.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/core/widgets/j_back_button.dart';
 import 'package:jini/presentation/core/widgets/j_background.dart';
@@ -14,59 +13,49 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final bloc = BlocProvider.of<SignUpBloc>(context);
 
-    return BlocListener<SignUpBloc, SignUpState>(
-      listener: (context, state) {
-        state.authFailureOrSuccess.fold(
-          () {},
-          (either) => either.fold(
-            (f) {
-              Get.snackbar(
-                'Sign Up Failed',
-                f.maybeMap(
-                  orElse: () => '',
-                  emailInUse: (_) => JErrorMessages.emailInUse,
-                  serverError: (_) => JErrorMessages.serverError,
+    return BlocBuilder<SignUpBloc, SignUpState>(
+      bloc: bloc,
+      buildWhen: (p, c) => p.isSubmitting != c.isSubmitting,
+      builder: (context, state) {
+        return JPage(
+          loading: bloc.state.isSubmitting,
+          child: Scaffold(
+            extendBodyBehindAppBar: true,
+            appBar: AppBar(
+              leading: const JBackButton(),
+            ),
+            body: Stack(
+              alignment: Alignment.center,
+              children: [
+                const JBackground(),
+                Center(
+                  child: SingleChildScrollView(
+                    padding: JScreenUtil.globalPadding,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        Text(
+                          'Create a new Account',
+                          style: textTheme.headlineLarge,
+                        ),
+                        JScreenUtil.vSpace(4),
+                        Text(
+                          'Sign up to create your new account.',
+                          style: textTheme.titleMedium,
+                        ),
+                        JScreenUtil.vSpace(30),
+                        const SignUpForm(),
+                      ],
+                    ),
+                  ),
                 ),
-              );
-            },
-            (r) {},
+              ],
+            ),
           ),
         );
       },
-      child: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          leading: const JBackButton(),
-        ),
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            const JBackground(),
-            Center(
-              child: SingleChildScrollView(
-                padding: JScreenUtil.globalPadding,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Text(
-                      'Create a new Account',
-                      style: textTheme.headlineLarge,
-                    ),
-                    JScreenUtil.vSpace(4),
-                    Text(
-                      'Sign up to create your new account.',
-                      style: textTheme.titleMedium,
-                    ),
-                    JScreenUtil.vSpace(30),
-                    const SignUpForm(),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
