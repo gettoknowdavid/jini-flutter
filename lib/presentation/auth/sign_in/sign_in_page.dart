ef4c24c5e254e6381fafd7eb9456587726d5f378
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 00:05:31 +0100

    Update sign_in_page.dart

--- lib/presentation/auth/sign_in/sign_in_page.dart
+++ lib/presentation/auth/sign_in/sign_in_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
+import 'package:jini/application/auth/auth_bloc.dart';
+import 'package:jini/application/auth/sign_in/sign_in_bloc.dart';
 import 'package:jini/presentation/auth/sign_in/widgets/sign_in_form.dart';
+import 'package:jini/presentation/core/common/j_page.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/core/widgets/j_background.dart';
 import 'package:jini/presentation/core/widgets/j_theme_switch.dart';
@@ -12,48 +16,58 @@ class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final bloc = BlocProvider.of<SignInBloc>(context);
+    bool loading = false;
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        actions: [
-          const JThemeSwitch(),
-        ],
-      ),
-      body: Stack(
-        alignment: Alignment.center,
-        children: [
-          const JBackground(),
-          Center(
-            child: SingleChildScrollView(
-              padding: JScreenUtil.globalPadding,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text('Welcome \nBack!', style: textTheme.headlineLarge),
-                  JScreenUtil.vSpace(4),
-                  Text(
-                    'Welcome back. You have been missed. \nPlease, sign into your account.',
-                    style: textTheme.titleMedium,
-                  ),
-                  JScreenUtil.vSpace(30),
-                  const SignInForm(),
-                  JScreenUtil.vSpace(16),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('Don\'t have an account?'),
-                      TextButton(
-                        onPressed: () => Get.toNamed(JRoutes.signUp),
-                        child: Text('Sign Up'),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
+    return BlocListener<SignInBloc, SignInState>(
+      bloc: bloc,
+      listenWhen: (p, c) => p.isSubmitting != c.isSubmitting,
+      listener: (context, state) => loading = state.isSubmitting,
+      child: JPage(
+        loading: loading,
+        child: Scaffold(
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            actions: [
+              const JThemeSwitch(),
+            ],
           ),
-        ],
+          body: Stack(
+            alignment: Alignment.center,
+            children: [
+              const JBackground(),
+              Center(
+                child: SingleChildScrollView(
+                  padding: JScreenUtil.globalPadding,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('Welcome \nBack!', style: textTheme.headlineLarge),
+                      JScreenUtil.vSpace(4),
+                      Text(
+                        'Welcome back. You have been missed. \nPlease, sign into your account.',
+                        style: textTheme.titleMedium,
+                      ),
+                      JScreenUtil.vSpace(30),
+                      const SignInForm(),
+                      JScreenUtil.vSpace(16),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text('Don\'t have an account?'),
+                          TextButton(
+                            onPressed: () => Get.toNamed(JRoutes.signUp),
+                            child: Text('Sign Up'),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
