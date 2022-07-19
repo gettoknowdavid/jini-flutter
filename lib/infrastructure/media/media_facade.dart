29b3e3eeacc8703fcf2c14a40b561848fd24e8fc
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 00:05:29 +0100

    Update media_facade.dart

--- lib/infrastructure/media/media_facade.dart
+++ lib/infrastructure/media/media_facade.dart
@@ -1,24 +1,55 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
 import 'package:jini/domain/media/i_media_facade.dart';
+import 'package:jini/domain/media/media_failure.dart';
 
 @LazySingleton(as: IMediaFacade)
 class MediaFacade implements IMediaFacade {
-  MediaFacade(this._picker);
+  MediaFacade(this._firebaseAuth, this._storage, this._picker);
 
+  final FirebaseAuth _firebaseAuth;
+  final FirebaseStorage _storage;
   final ImagePicker _picker;
 
   @override
-  Future<Option<File?>> getImage() async {
+  Future<File?> getImage() async {
     final XFile? _file = await _picker.pickImage(source: ImageSource.gallery);
 
     if (_file != null) {
-      return optionOf(File(_file.path));
+      return File(_file.path);
     } else {
-      return optionOf(null);
+      return null;
+    }
+  }
+
+  Reference ref() {
+    final uid = _firebaseAuth.currentUser!.uid;
+    return _storage.ref().child("images/avatar/$uid.jpg");
+  }
+
+  @override
+  Future<Either<MediaFailure, Unit>> upload(String path) async {
+    File _file = File(path);
+    try {
+      await ref().putFile(_file);
+      return right(unit);
+    } on FirebaseException catch (_) {
+      return left(MediaFailure.serverError());
+    }
+  }
+
+  @override
+  Future<Either<MediaFailure, String>> download() async {
+    try {
+      final _r = await ref().getDownloadURL();
+      return right(_r);
+    } on FirebaseException catch (_) {
+      return left(MediaFailure.serverError());
     }
   }
 }
