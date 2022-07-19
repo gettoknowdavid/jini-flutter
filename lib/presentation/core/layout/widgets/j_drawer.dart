b35ac49f6f87ad88874a02f921d517df614eab38
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 00:05:44 +0100

    Update j_drawer.dart

--- lib/presentation/core/layout/widgets/j_drawer.dart
+++ lib/presentation/core/layout/widgets/j_drawer.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:jini/application/profile/profile_bloc.dart';
 import 'package:jini/infrastructure/auth/j_user_dtos.dart';
+import 'package:jini/presentation/core/common/j_avatar.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/core/layout/widgets/drawer_list.dart';
 import 'package:jini/presentation/core/routes/j_router.dart';
@@ -40,37 +41,27 @@ class JDrawer extends StatelessWidget {
                 builder: (context, state) {
                   final user = JUserDto.fromDomain(bloc.state.user);
 
-                  return Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Get.close(1);
-                          Get.toNamed(JRoutes.profilePage);
-                        },
-                        child: CircleAvatar(
-                          radius: JScreenUtil.sw(0.07),
-                          child: Text('DM'),
-                        ),
-                      ),
-                      JScreenUtil.hSpace(10),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            user.name!,
-                            style: textTheme.titleMedium!.copyWith(
-                              color: isDark ? Colors.white : Colors.black,
-                              fontWeight: FontWeight.w600,
+                  return GestureDetector(
+                    onTap: () => Get.toNamed(JRoutes.profilePage),
+                    child: Row(
+                      children: [
+                        const JAvatar(isForProfile: false, radius: 0.07),
+                        JScreenUtil.hSpace(14),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              user.name!,
+                              style: textTheme.bodyText1!.copyWith(
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                          ),
-                          JScreenUtil.vSpace(2),
-                          Text(
-                            user.email!,
-                            style: textTheme.caption,
-                          ),
-                        ],
-                      ),
-                    ],
+                            JScreenUtil.vSpace(2),
+                            Text(user.email!, style: textTheme.caption),
+                          ],
+                        ),
+                      ],
+                    ),
                   );
                 },
               ),
