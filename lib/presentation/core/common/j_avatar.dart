aa1fed74bc45222c5b565f86f0f6617a69b53dfd
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 00:05:39 +0100

    Update j_avatar.dart

--- lib/presentation/core/common/j_avatar.dart
+++ lib/presentation/core/common/j_avatar.dart
@@ -4,29 +4,26 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jini/application/profile/profile_bloc.dart';
 import 'package:jini/domain/core/user_type.dart';
 import 'package:jini/infrastructure/auth/j_user_dtos.dart';
-import 'package:jini/presentation/core/common/j_icons.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class JAvatar extends StatelessWidget {
-  const JAvatar({Key? key}) : super(key: key);
+  const JAvatar({
+    Key? key,
+    this.isForProfile = true,
+    this.radius = 0.185,
+  }) : super(key: key);
+
+  final double radius;
+  final bool isForProfile;
 
   @override
   Widget build(BuildContext context) {
-    final theme = Theme.of(context);
+    final textTheme = Theme.of(context).textTheme;
     final bloc = BlocProvider.of<ProfileBloc>(context);
 
-    return BlocConsumer<ProfileBloc, ProfileState>(
+    return BlocBuilder<ProfileBloc, ProfileState>(
       bloc: bloc,
-      listenWhen: (p, c) {
-        return p.saveFailureOrSuccessOption != c.saveFailureOrSuccessOption;
-      },
-      listener: (context, state) {
-        state.saveFailureOrSuccessOption.fold(
-          () => null,
-          (a) => a.fold((l) => null, (r) => null),
-        );
-      },
       buildWhen: (p, c) => p.isEditing != c.isEditing,
       builder: (context, state) {
         final user = JUserDto.fromDomain(bloc.state.user);
@@ -34,43 +31,49 @@ class JAvatar extends StatelessWidget {
 
         return Stack(
           clipBehavior: Clip.none,
+          alignment: Alignment.center,
           children: [
-            Parent(
-              gesture: Gestures()
-                ..onTap(isEditing
-                    ? () => bloc.add(const ProfileEvent.avatarChanged())
-                    : () {}),
-              style: ParentStyle()
-                ..height(JScreenUtil.sw(0.35))
-                ..width(JScreenUtil.sw(0.35))
-                ..borderRadius(all: JScreenUtil.radius(34))
-                ..elevation(14, opacity: 0.4)
-                ..alignmentContent.center()
-                ..background.color(theme.primaryColor),
-              child: user.avatar != null
-                  ?isEditing? FileImage(user.avatar!)
-                  : Txt(
-                      user.name != null ? user.name![0] : 'JI',
-                      style: TxtStyle()
-                        ..textColor(Colors.white)
-                        ..fontSize(JScreenUtil.fontSize(36))
-                        ..textAlign.center(),
-                    ),
+            GestureDetector(
+              onTap: (isForProfile && isEditing)
+                  ? () => bloc.add(const ProfileEvent.avatarChanged())
+                  : null,
+              child: CircleAvatar(
+                radius: JScreenUtil.sw(radius + 0.005),
+                backgroundColor: Colors.white,
+                child: CircleAvatar(
+                  radius: JScreenUtil.sw(radius),
+                  backgroundImage: bloc.state.avatarFile == null
+                      ? null
+                      : FileImage(bloc.state.avatarFile!),
+                  foregroundImage:
+                      user.avatar == null ? null : NetworkImage(user.avatar!),
+                  child: user.avatar != null || bloc.state.avatarFile != null
+                      ? const SizedBox()
+                      : Text(
+                          user.name![0].toUpperCase(),
+                          style: textTheme.headlineLarge?.copyWith(
+                            color: Colors.white,
+                            fontSize: JScreenUtil.fontSize(radius * 250),
+                          ),
+                        ),
+                ),
+              ),
             ),
-            if (isEditing)
+            if (bloc.state.isSaving) const CircularProgressIndicator.adaptive(),
+            if (isEditing && isForProfile)
               Positioned(
-                bottom: JScreenUtil.w(-5),
-                right: JScreenUtil.w(-10),
+                bottom: JScreenUtil.w(1),
+                right: JScreenUtil.w(5),
                 child: Parent(
                   style: ParentStyle()
                     ..circle()
                     ..padding(all: JScreenUtil.r(8))
-                    ..background.color(Colors.white54)
+                    ..background.color(Colors.white)
                     ..alignmentContent.center(),
-                  child: Icon(PhosphorIcons.pencil, size: JScreenUtil.r(20)),
+                  child: Icon(PhosphorIcons.pencil, color: Colors.black87),
                 ),
               ),
-            if (!isEditing)
+            if (!isEditing && isForProfile)
               Positioned(
                 bottom: JScreenUtil.w(-5),
                 right: JScreenUtil.w(-10),
