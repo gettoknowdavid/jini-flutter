ea71c8802efbf09c759d41c6376fad9dd48413cd
David Michael II <knowdavidmichael@gmail.com>
2022-07-20 03:27:24 +0100

    fix: lint errors

--- lib/infrastructure/auth/j_user_dtos.dart
+++ lib/infrastructure/auth/j_user_dtos.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
