27529110b7f5d28cd41b1a3d701f9267977b7bb5
David Michael II <knowdavidmichael@gmail.com>
2022-07-21 02:26:19 +0100

    feat: add source parameter to getImage in IMediaFacade

--- lib/domain/media/i_media_facade.dart
+++ lib/domain/media/i_media_facade.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:jini/domain/media/media_failure.dart';
 
 abstract class IMediaFacade {
-  Future<File?> getImage();
+  Future<File?> getImage([ImageSource source = ImageSource.gallery]);
 
   Future<Either<MediaFailure, Unit>> upload(String path);
 
