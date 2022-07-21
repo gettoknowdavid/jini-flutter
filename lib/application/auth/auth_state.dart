97f69fa62e871e7b35eca84aa89c20cd8f81bf3e
David Michael II <knowdavidmichael@gmail.com>
2022-07-21 03:07:13 +0100

    fix: remove Auth loading state

--- lib/application/auth/auth_state.dart
+++ lib/application/auth/auth_state.dart
@@ -3,7 +3,7 @@ part of 'auth_bloc.dart';
 @freezed
 class AuthState with _$AuthState {
   const factory AuthState.initial() = Initial;
-  const factory AuthState.loading() = AuthLoading;
+  // const factory AuthState.loading() = AuthLoading;
   const factory AuthState.notLoading() = AuthNotLoading;
   const factory AuthState.authenticated() = Authenticated;
   const factory AuthState.unauthenticated() = Unauthenticated;
