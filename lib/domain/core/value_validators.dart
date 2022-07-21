859cbaf4dbf7dde21eae237a51e726abe6a0d9ad
David Michael II <knowdavidmichael@gmail.com>
2022-07-21 02:55:22 +0100

    fix: lint errors in value_validators.dart

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,24 +1,14 @@
-import 'dart:io';
-
 import 'package:dartz/dartz.dart';
 import 'package:jini/domain/core/blood_group.dart';
 import 'package:jini/domain/core/failures.dart';
 import 'package:jini/domain/core/gender.dart';
 import 'package:jini/domain/core/user_type.dart';
-import 'package:mime/mime.dart';
 
-Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
-  if (input.isEmpty) {
-    return left(ValueFailure.empty(failedValue: input));
-  } else {
-    return right(input);
-  }
-}
-
-Either<ValueFailure<UserType>, UserType> validateUserTypeNotNull(
-    UserType? input) {
-  if (input == null) {
-    return left(ValueFailure.nullValue());
+Either<ValueFailure<num>, num> validateAge(num input) {
+  if (input.toInt() < 17) {
+    return left(ValueFailure.tooYoung(failedValue: input));
+  } else if (input.toInt() > 60) {
+    return left(ValueFailure.tooOld(failedValue: input));
   } else {
     return right(input);
   }
@@ -33,9 +23,14 @@ Either<ValueFailure<BloodGroup>, BloodGroup> validateBloodGroupNotNull(
   }
 }
 
-Either<ValueFailure<Gender>, Gender> validateGenderNotNull(Gender? input) {
-  if (input == null) {
-    return left(ValueFailure.nullValue());
+Either<ValueFailure<DateTime>, DateTime> validateDOB(DateTime input) {
+  final present = DateTime.now().year;
+  final difference = present - input.year;
+
+  if (difference < 17) {
+    return left(ValueFailure.tooYoung(failedValue: input));
+  } else if (difference > 60) {
+    return left(ValueFailure.tooOld(failedValue: input));
   } else {
     return right(input);
   }
@@ -55,13 +50,11 @@ Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   }
 }
 
-Either<ValueFailure<String>, String> validatePhone(String input) {
-  const phoneRegex =
-      r"""^(\+234|234|0)(70|70|70|70|70|70|70|70|70|80|80|80|80|80|80|80|80|81|81|81|81|81|81|81|81|81|81|91|90|90|90|90|90|90|90|90|90)([0-9]{8})$""";
-  if (RegExp(phoneRegex).hasMatch(input)) {
-    return right(input);
+Either<ValueFailure<Gender>, Gender> validateGenderNotNull(Gender? input) {
+  if (input == null) {
+    return left(ValueFailure.nullValue());
   } else {
-    return left(ValueFailure.invalidPhone(failedValue: input));
+    return right(input);
   }
 }
 
@@ -82,24 +75,28 @@ Either<ValueFailure<String>, String> validatePassword(String input) {
   }
 }
 
-Either<ValueFailure<num>, num> validateAge(num input) {
-  if (input.toInt() < 17) {
-    return left(ValueFailure.tooYoung(failedValue: input));
-  } else if (input.toInt() > 60) {
-    return left(ValueFailure.tooOld(failedValue: input));
+Either<ValueFailure<String>, String> validatePhone(String input) {
+  const phoneRegex =
+      r"""^(\+234|234|0)(70|70|70|70|70|70|70|70|70|80|80|80|80|80|80|80|80|81|81|81|81|81|81|81|81|81|81|91|90|90|90|90|90|90|90|90|90)([0-9]{8})$""";
+  if (RegExp(phoneRegex).hasMatch(input)) {
+    return right(input);
+  } else {
+    return left(ValueFailure.invalidPhone(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isEmpty) {
+    return left(ValueFailure.empty(failedValue: input));
   } else {
     return right(input);
   }
 }
 
-Either<ValueFailure<DateTime>, DateTime> validateDOB(DateTime input) {
-  final present = DateTime.now().year;
-  final difference = present - input.year;
-
-  if (difference < 17) {
-    return left(ValueFailure.tooYoung(failedValue: input));
-  } else if (difference > 60) {
-    return left(ValueFailure.tooOld(failedValue: input));
+Either<ValueFailure<UserType>, UserType> validateUserTypeNotNull(
+    UserType? input) {
+  if (input == null) {
+    return left(ValueFailure.nullValue());
   } else {
     return right(input);
   }
