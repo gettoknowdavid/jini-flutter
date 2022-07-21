14b5ad2bf2c7416eb6882f064da05c7dd0c4f3a0
David Michael II <knowdavidmichael@gmail.com>
2022-07-21 03:17:28 +0100

    feat: create GenderGrid widget

---
+++ lib/presentation/profile/widgets/gender_grid.dart
@@ -0,0 +1,64 @@
+import 'package:division/division.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jini/application/profile/profile_bloc.dart';
+import 'package:jini/domain/core/gender.dart';
+import 'package:jini/presentation/core/common/j_screen_util.dart';
+
+class GenderGrid extends StatelessWidget {
+  const GenderGrid({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final bloc = BlocProvider.of<ProfileBloc>(context);
+
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      bloc: bloc,
+      listenWhen: (p, c) => p.saveOption != c.saveOption,
+      listener: (context, state) {
+        state.saveOption.fold(
+          () => null,
+          (a) => a.fold((l) => null, (r) => null),
+        );
+      },
+      buildWhen: (p, c) => p.user.gender != c.user.gender,
+      builder: (context, state) {
+        return GridView.builder(
+          primary: false,
+          shrinkWrap: true,
+          itemCount: Gender.values.length,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisExtent: JScreenUtil.sh(0.07),
+            crossAxisSpacing: JScreenUtil.r(10),
+            mainAxisSpacing: JScreenUtil.r(10),
+          ),
+          itemBuilder: (context, i) {
+            final _gender = Gender.values[i];
+            final _cGender = bloc.state.user.gender;
+            final selected =
+                _cGender == null ? false : _gender == _cGender.getOrCrash();
+            return Parent(
+              gesture: Gestures()
+                ..onTap(() => bloc.add(ProfileEvent.genderChanged(_gender))),
+              style: ParentStyle()
+                ..alignmentContent.center()
+                ..borderRadius(all: JScreenUtil.r(10))
+                ..padding(all: JScreenUtil.r(14))
+                ..background
+                    .color(selected ? theme.primaryColor : theme.disabledColor),
+              child: Text(
+                _gender.value,
+                style: textTheme.bodyLarge?.copyWith(
+                  color: selected ? Colors.white : textTheme.bodyLarge?.color,
+                ),
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
