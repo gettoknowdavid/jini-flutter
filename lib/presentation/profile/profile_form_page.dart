c4f8e839d5579dd966b9bce9b8801abdae157d16
David Michael II <knowdavidmichael@gmail.com>
2022-07-21 02:38:56 +0100

    refactor: stepper_controls widget

--- lib/presentation/profile/profile_form_page.dart
+++ lib/presentation/profile/profile_form_page.dart
@@ -13,6 +13,7 @@ import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/core/routes/j_router.dart';
 import 'package:jini/presentation/core/widgets/j_text_form_field.dart';
 import 'package:jini/presentation/core/widgets/j_theme_switch.dart';
+import 'package:jini/presentation/profile/widgets/stepper_controls.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class BloodGroupGrid extends StatelessWidget {
@@ -426,66 +427,6 @@ class _ProfileFormPageState extends State<ProfileFormPage> {
           ),
         ),
       ),
-    );
-  }
-}
-
-class StepperControls extends StatelessWidget {
-  const StepperControls({
-    Key? key,
-    required this.controls,
-    required this.isLastStep,
-  }) : super(key: key);
-
-  final ControlsDetails controls;
-  final bool isLastStep;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-    final bloc = BlocProvider.of<ProfileBloc>(context);
-    int _index = 0;
-
-    return BlocConsumer<ProfileBloc, ProfileState>(
-      bloc: bloc,
-      listenWhen: (p, c) => p.activeStepIndex != c.activeStepIndex,
-      listener: (context, state) => _index = state.activeStepIndex,
-      buildWhen: (p, c) => p.activeStepIndex != c.activeStepIndex,
-      builder: (context, state) {
-        return Container(
-          padding: JScreenUtil.padSymmetric(vertical: 20),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              if (_index > 0)
-                Parent(
-                  style: ParentStyle()
-                    ..circle()
-                    ..alignmentContent.center()
-                    ..background.color(theme.primaryColor),
-                  child: IconButton(
-                    onPressed: controls.onStepCancel,
-                    icon: Icon(PhosphorIcons.arrowBendUpLeft),
-                  ),
-                ),
-              JScreenUtil.hSpace(20),
-              Parent(
-                style: ParentStyle()
-                  ..circle()
-                  ..alignmentContent.center()
-                  ..background.color(theme.primaryColor),
-                child: IconButton(
-                  onPressed: controls.onStepContinue,
-                  icon: Icon(isLastStep
-                      ? PhosphorIcons.check
-                      : PhosphorIcons.arrowBendUpRight),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
