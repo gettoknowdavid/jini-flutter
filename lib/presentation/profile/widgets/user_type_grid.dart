09cde418a5bd63bd09f384fb5c1f452ec76ceae2
David Michael II <knowdavidmichael@gmail.com>
2022-07-21 18:56:11 +0100

    fix: lint errors in user_type_grid.dart

--- lib/presentation/profile/widgets/user_type_grid.dart
+++ lib/presentation/profile/widgets/user_type_grid.dart
@@ -8,6 +8,47 @@ import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/core/widgets/j_button.dart';
 import 'package:jini/presentation/profile/widgets/profile_blood_group_widget.dart';
 
+class EditUserTypeBottomSheet extends StatelessWidget {
+  const EditUserTypeBottomSheet({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = BlocProvider.of<ProfileBloc>(context);
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+
+    _handleSave() => bloc.add(ProfileEvent.profileUpdated());
+
+    return BlocConsumer<ProfileBloc, ProfileState>(
+      bloc: bloc,
+      listenWhen: (p, c) => p.saveOption != c.saveOption,
+      listener: (context, state) {
+        state.saveOption.fold(
+          () => null,
+          (a) => a.fold((l) => null, (r) => Get.close(1)),
+        );
+      },
+      buildWhen: (p, c) => p.isSaving != c.isSaving,
+      builder: (context, state) => Parent(
+        style: sheetStyle(theme),
+        child: Wrap(
+          alignment: WrapAlignment.center,
+          runSpacing: JScreenUtil.r(30),
+          children: <Widget>[
+            Text('Update your account type', style: textTheme.titleLarge),
+            const UserTypeGrid(),
+            JButton(
+              title: 'Save',
+              onPressed: _handleSave,
+              loading: bloc.state.isSaving,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class UserTypeGrid extends StatelessWidget {
   const UserTypeGrid({Key? key}) : super(key: key);
 
@@ -65,47 +106,6 @@ class UserTypeGrid extends StatelessWidget {
           },
         );
       },
-    );
-  }
-}
-
-class EditUserTypeBottomSheet extends StatelessWidget {
-  const EditUserTypeBottomSheet({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final bloc = BlocProvider.of<ProfileBloc>(context);
-    final theme = Theme.of(context);
-    final textTheme = theme.textTheme;
-
-    _handleSave() => bloc.add(ProfileEvent.profileUpdated());
-
-    return BlocConsumer<ProfileBloc, ProfileState>(
-      bloc: bloc,
-      listenWhen: (p, c) => p.saveOption != c.saveOption,
-      listener: (context, state) {
-        state.saveOption.fold(
-          () => null,
-          (a) => a.fold((l) => null, (r) => Get.close(1)),
-        );
-      },
-      buildWhen: (p, c) => p.isSaving != c.isSaving,
-      builder: (context, state) => Parent(
-        style: sheetStyle(theme),
-        child: Wrap(
-          alignment: WrapAlignment.center,
-          runSpacing: JScreenUtil.r(30),
-          children: <Widget>[
-            Text('Update your account type', style: textTheme.titleLarge),
-            const UserTypeGrid(),
-            JButton(
-              title: 'Save',
-              onPressed: _handleSave,
-              loading: bloc.state.isSaving,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
