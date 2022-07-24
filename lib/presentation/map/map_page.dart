47a89de9b8bfb8b1c0589a57b0747653cea51b1d
David Michael II <knowdavidmichael@gmail.com>
2022-07-24 20:22:57 +0100

    feat: create MapPage

---
+++ lib/presentation/map/map_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:jini/presentation/core/common/j_page.dart';
+
+class MapPage extends StatelessWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return JPage(
+      child: Container(),
+    );
+  }
+}
