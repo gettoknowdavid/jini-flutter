87fd46934a8f23fca7c1b0482125b379ca31dffe
David Michael II <knowdavidmichael@gmail.com>
2022-07-24 20:15:24 +0100

    Update profile_bloc.dart

--- lib/application/profile/profile_bloc.dart
+++ lib/application/profile/profile_bloc.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:image_picker/image_picker.dart';
@@ -12,6 +13,7 @@ import 'package:jini/domain/auth/value_objects.dart';
 import 'package:jini/domain/core/blood_group.dart';
 import 'package:jini/domain/core/gender.dart';
 import 'package:jini/domain/core/user_type.dart';
+import 'package:jini/domain/location/i_location_facade.dart';
 import 'package:jini/domain/media/i_media_facade.dart';
 
 part 'profile_event.dart';
@@ -20,10 +22,8 @@ part 'profile_bloc.freezed.dart';
 
 @injectable
 class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
-  ProfileBloc(
-    this._authFacade,
-    this._mediaFacade,
-  ) : super(ProfileState.initial()) {
+  ProfileBloc(this._authFacade, this._mediaFacade, this._locationFacade)
+      : super(ProfileState.initial()) {
     on<_ProfileEditPressed>((event, emit) => _editPressed(event, emit));
     on<_Initialized>((event, emit) => _initialized(event, emit));
     on<_NameChanged>((event, emit) => _nameChanged(event, emit));
@@ -42,10 +42,12 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
     on<_UpdateStepIndex>((event, emit) => _updateStepIndex(event, emit));
     on<_StepForward>((event, emit) => _stepForward(event, emit));
     on<_StepBackward>((event, emit) => _stepBackward(event, emit));
+    on<_LocationChanged>((event, emit) => _locationChanged(event, emit));
   }
 
   final IAuthFacade _authFacade;
   final IMediaFacade _mediaFacade;
+  final ILocationFacade _locationFacade;
 
   _nameChanged(_NameChanged e, Emitter<ProfileState> emit) async {
     emit(
@@ -252,5 +254,39 @@ class ProfileBloc extends Bloc<ProfileEvent, ProfileState> {
         activeStepIndex: e.step,
       ),
     );
+  }
+
+  _locationChanged(_LocationChanged e, Emitter<ProfileState> emit) async {
+    Either<AuthFailure, Unit> failureOrSuccess;
+    await _locationFacade.requestPermission();
+    final _geoEither = await _locationFacade.getLocation();
+
+    final _placeEither = await _locationFacade.getCityCountry(e.geo);
+
+    _geoEither.fold(
+      (l) => state,
+      (geo) {
+        emit(
+          state.copyWith(user: state.user.copyWith(location: ILocation(geo))),
+        );
+      },
+    );
+
+    _placeEither.fold(
+      (l) => state,
+      (place) {
+        emit(state.copyWith(user: state.user.copyWith(city: place)));
+      },
+    );
+
+    print(state.user.location);
+
+    failureOrSuccess = await _authFacade.updateUser(state.user);
+
+    emit(state.copyWith(
+      isSaving: false,
+      showErrorMessages: true,
+      saveOption: optionOf(failureOrSuccess),
+    ));
   }
 }
