a150eec358c3852a2bee4a2fafd40cf5df3358d0
David Michael II <knowdavidmichael@gmail.com>
2022-07-24 20:16:12 +0100

    feat: u[date di

--- lib/di/register_module.dart
+++ lib/di/register_module.dart
@@ -1,12 +1,16 @@
 import 'package:image_picker/image_picker.dart';
 import 'package:injectable/injectable.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:location/location.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 @module
 abstract class RegisterModule {
   @lazySingleton
   ImagePicker get imagePicker => ImagePicker();
+
+  @lazySingleton
+  Location get location => Location();
 
   @lazySingleton
   InternetConnectionChecker get connection => InternetConnectionChecker();
