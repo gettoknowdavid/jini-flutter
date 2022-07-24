b56cc1101281ca0c9e5a9048b6db00d53ec5bf78
David Michael II <knowdavidmichael@gmail.com>
2022-07-24 20:17:23 +0100

    feat: add ILocationFacade

--- lib/domain/location/i_location_facade.dart
+++ lib/domain/location/i_location_facade.dart
@@ -1,1 +1,9 @@
-abstract class ILocationFacade {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:jini/domain/location/location_failure.dart';
+
+abstract class ILocationFacade {
+  Future<Either<LocationFailure, Unit>> requestPermission();
+  Future<Either<LocationFailure, GeoPoint?>> getLocation();
+  Future<Either<LocationFailure, String?>> getCityCountry([GeoPoint? geo]);
+}
