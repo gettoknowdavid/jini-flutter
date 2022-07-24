de9761e7d1e84481e5abff72c7a5cfc8386383f4
David Michael II <knowdavidmichael@gmail.com>
2022-07-24 20:17:40 +0100

    Create location_failure.freezed.dart

---
+++ lib/domain/location/location_failure.freezed.dart
@@ -0,0 +1,284 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'location_failure.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$LocationFailure {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() permissionDenied,
+    required TResult Function() unknownLocation,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? permissionDenied,
+    TResult Function()? unknownLocation,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? permissionDenied,
+    TResult Function()? unknownLocation,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(PermissionDenied value) permissionDenied,
+    required TResult Function(UnknownLocation value) unknownLocation,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(PermissionDenied value)? permissionDenied,
+    TResult Function(UnknownLocation value)? unknownLocation,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(PermissionDenied value)? permissionDenied,
+    TResult Function(UnknownLocation value)? unknownLocation,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LocationFailureCopyWith<$Res> {
+  factory $LocationFailureCopyWith(
+          LocationFailure value, $Res Function(LocationFailure) then) =
+      _$LocationFailureCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class _$LocationFailureCopyWithImpl<$Res>
+    implements $LocationFailureCopyWith<$Res> {
+  _$LocationFailureCopyWithImpl(this._value, this._then);
+
+  final LocationFailure _value;
+  // ignore: unused_field
+  final $Res Function(LocationFailure) _then;
+}
+
+/// @nodoc
+abstract class _$$PermissionDeniedCopyWith<$Res> {
+  factory _$$PermissionDeniedCopyWith(
+          _$PermissionDenied value, $Res Function(_$PermissionDenied) then) =
+      __$$PermissionDeniedCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$PermissionDeniedCopyWithImpl<$Res>
+    extends _$LocationFailureCopyWithImpl<$Res>
+    implements _$$PermissionDeniedCopyWith<$Res> {
+  __$$PermissionDeniedCopyWithImpl(
+      _$PermissionDenied _value, $Res Function(_$PermissionDenied) _then)
+      : super(_value, (v) => _then(v as _$PermissionDenied));
+
+  @override
+  _$PermissionDenied get _value => super._value as _$PermissionDenied;
+}
+
+/// @nodoc
+
+class _$PermissionDenied implements PermissionDenied {
+  const _$PermissionDenied();
+
+  @override
+  String toString() {
+    return 'LocationFailure.permissionDenied()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$PermissionDenied);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() permissionDenied,
+    required TResult Function() unknownLocation,
+  }) {
+    return permissionDenied();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? permissionDenied,
+    TResult Function()? unknownLocation,
+  }) {
+    return permissionDenied?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? permissionDenied,
+    TResult Function()? unknownLocation,
+    required TResult orElse(),
+  }) {
+    if (permissionDenied != null) {
+      return permissionDenied();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(PermissionDenied value) permissionDenied,
+    required TResult Function(UnknownLocation value) unknownLocation,
+  }) {
+    return permissionDenied(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(PermissionDenied value)? permissionDenied,
+    TResult Function(UnknownLocation value)? unknownLocation,
+  }) {
+    return permissionDenied?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(PermissionDenied value)? permissionDenied,
+    TResult Function(UnknownLocation value)? unknownLocation,
+    required TResult orElse(),
+  }) {
+    if (permissionDenied != null) {
+      return permissionDenied(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class PermissionDenied implements LocationFailure {
+  const factory PermissionDenied() = _$PermissionDenied;
+}
+
+/// @nodoc
+abstract class _$$UnknownLocationCopyWith<$Res> {
+  factory _$$UnknownLocationCopyWith(
+          _$UnknownLocation value, $Res Function(_$UnknownLocation) then) =
+      __$$UnknownLocationCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$UnknownLocationCopyWithImpl<$Res>
+    extends _$LocationFailureCopyWithImpl<$Res>
+    implements _$$UnknownLocationCopyWith<$Res> {
+  __$$UnknownLocationCopyWithImpl(
+      _$UnknownLocation _value, $Res Function(_$UnknownLocation) _then)
+      : super(_value, (v) => _then(v as _$UnknownLocation));
+
+  @override
+  _$UnknownLocation get _value => super._value as _$UnknownLocation;
+}
+
+/// @nodoc
+
+class _$UnknownLocation implements UnknownLocation {
+  const _$UnknownLocation();
+
+  @override
+  String toString() {
+    return 'LocationFailure.unknownLocation()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$UnknownLocation);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() permissionDenied,
+    required TResult Function() unknownLocation,
+  }) {
+    return unknownLocation();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult Function()? permissionDenied,
+    TResult Function()? unknownLocation,
+  }) {
+    return unknownLocation?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? permissionDenied,
+    TResult Function()? unknownLocation,
+    required TResult orElse(),
+  }) {
+    if (unknownLocation != null) {
+      return unknownLocation();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(PermissionDenied value) permissionDenied,
+    required TResult Function(UnknownLocation value) unknownLocation,
+  }) {
+    return unknownLocation(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult Function(PermissionDenied value)? permissionDenied,
+    TResult Function(UnknownLocation value)? unknownLocation,
+  }) {
+    return unknownLocation?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(PermissionDenied value)? permissionDenied,
+    TResult Function(UnknownLocation value)? unknownLocation,
+    required TResult orElse(),
+  }) {
+    if (unknownLocation != null) {
+      return unknownLocation(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class UnknownLocation implements LocationFailure {
+  const factory UnknownLocation() = _$UnknownLocation;
+}
