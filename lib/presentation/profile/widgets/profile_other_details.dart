274a623222059be8403a3957df7eb407102e57b9
David Michael II <knowdavidmichael@gmail.com>
2022-07-25 18:11:30 +0100

    refactor: bottom sheets to JBottomSheets

--- lib/presentation/profile/widgets/profile_other_details.dart
+++ lib/presentation/profile/widgets/profile_other_details.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jini/presentation/core/common/date_formatter.dart';
-import 'package:jini/presentation/profile/widgets/date_of_birth_field.dart';
-import 'package:jini/presentation/profile/widgets/gender_grid.dart';
-import 'package:jini/presentation/profile/widgets/phone_field.dart';
-import 'package:jini/presentation/profile/widgets/user_type_grid.dart';
+import 'package:jini/presentation/core/widgets/j_bottom_sheets.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 import 'package:jini/application/profile/profile_bloc.dart';
@@ -12,7 +9,6 @@ import 'package:jini/domain/core/gender.dart';
 import 'package:jini/domain/core/user_type.dart';
 import 'package:jini/infrastructure/auth/j_user_dtos.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
-import 'package:jini/presentation/profile/widgets/edit_bottom_sheet.dart';
 import 'package:jini/presentation/profile/widgets/profile_details_item.dart';
 
 class ProfileOtherDetails extends StatelessWidget {
@@ -41,60 +37,42 @@ class ProfileOtherDetails extends StatelessWidget {
               title: 'Account Type',
               icon: PhosphorIcons.user,
               value: user.userType!.value,
-              onTap: () => showBottomSheet(
-                context: context,
-                builder: (context) => const EditUserTypeBottomSheet(),
-              ),
+              onTap: () => JBottomSheets.editUserType(context),
             ),
             JScreenUtil.vSpace(10),
             ProfileDetailsItem(
               title: 'Phone',
               icon: PhosphorIcons.phone,
               value: user.phone,
-              onTap: () => showBottomSheet(
-                context: context,
-                builder: (context) => const EditPhoneBottomSheet(),
-              ),
+              onTap: () => JBottomSheets.editPhone(context),
             ),
             JScreenUtil.vSpace(10),
             ProfileDetailsItem(
               title: 'Gender',
               icon: PhosphorIcons.genderIntersex,
               value: user.gender?.value,
-              onTap: () => showBottomSheet(
-                context: context,
-                builder: (context) => const EditGenderBottomSheet(),
-              ),
+              onTap: () => JBottomSheets.editGender(context),
             ),
             JScreenUtil.divider,
             ProfileDetailsItem(
               title: 'Date of Birth',
               icon: PhosphorIcons.calendarBlank,
               value: user.dob == null ? null : dateFormat(user.dob!),
-              onTap: () => showBottomSheet(
-                context: context,
-                builder: (context) => const EditDOBBottomSheet(),
-              ),
+              onTap: () => JBottomSheets.editDateOfBirth(context),
             ),
             JScreenUtil.divider,
             ProfileDetailsItem(
               title: 'Height',
               icon: PhosphorIcons.arrowsVertical,
               value: user.height == 0 ? null : user.height?.toString(),
-              onTap: () => showBottomSheet(
-                context: context,
-                builder: (context) => const EditHeightBottomSheet(),
-              ),
+              onTap: () => JBottomSheets.editHeight(context),
             ),
             JScreenUtil.divider,
             ProfileDetailsItem(
               title: 'Weight',
               icon: PhosphorIcons.arrowsHorizontal,
               value: user.weight == null ? null : user.weight?.toString(),
-              onTap: () => showBottomSheet(
-                context: context,
-                builder: (context) => const EditWeightBottomSheet(),
-              ),
+              onTap: () => JBottomSheets.editWeight(context),
             ),
             JScreenUtil.vSpace(30)
           ],
