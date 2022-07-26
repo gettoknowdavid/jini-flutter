3de88d0632b16e97319d876f6e98d78df6fba6d3
David Michael II <knowdavidmichael@gmail.com>
2022-07-26 16:58:43 +0100

    fix: remove second app bar in event page

--- lib/presentation/event/event_page.dart
+++ lib/presentation/event/event_page.dart
@@ -5,10 +5,6 @@ class EventPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Event'),
-      ),
-    );
+    return Scaffold();
   }
 }
