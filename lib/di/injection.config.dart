f70ae19dc38eadf4b9334bc9a4fc3c89a7558d8d
David Michael II <knowdavidmichael@gmail.com>
2022-08-23 18:38:26 +0100

    Update injection.config.dart

--- lib/di/injection.config.dart
+++ lib/di/injection.config.dart
@@ -16,26 +16,28 @@ import 'package:location/location.dart' as _i14;
 import 'package:shared_preferences/shared_preferences.dart' as _i16;
 
 import '../application/auth/auth_bloc.dart' as _i19;
-import '../application/auth/forgot_password/forgot_password_bloc.dart' as _i20;
+import '../application/auth/forgot_password/forgot_password_bloc.dart' as _i22;
 import '../application/auth/sign_in/sign_in_bloc.dart' as _i17;
 import '../application/auth/sign_up/sign_up_bloc.dart' as _i18;
 import '../application/core/bottom_nav/bottom_nav_cubit.dart' as _i3;
-import '../application/core/settings/settings_bloc.dart' as _i28;
-import '../application/profile/profile_bloc.dart' as _i27;
+import '../application/core/settings/settings_bloc.dart' as _i30;
+import '../application/create_request/create_request_bloc.dart' as _i20;
+import '../application/profile/profile_bloc.dart' as _i29;
 import '../domain/auth/i_auth_facade.dart' as _i8;
-import '../domain/location/i_location_facade.dart' as _i21;
+import '../domain/location/i_location_facade.dart' as _i23;
 import '../domain/mail/i_mail_facade.dart' as _i10;
-import '../domain/media/i_media_facade.dart' as _i23;
-import '../domain/settings/i_settings_facade.dart' as _i25;
+import '../domain/media/i_media_facade.dart' as _i25;
+import '../domain/request/i_request_facade.dart' as _i21;
+import '../domain/settings/i_settings_facade.dart' as _i27;
 import '../infrastructure/auth/firebase_auth_facade.dart' as _i9;
 import '../infrastructure/auth/firebase_user_mapper.dart' as _i7;
-import '../infrastructure/core/firebase_injectable_module.dart' as _i29;
-import '../infrastructure/location/location_facade.dart' as _i22;
+import '../infrastructure/core/firebase_injectable_module.dart' as _i31;
+import '../infrastructure/location/location_facade.dart' as _i24;
 import '../infrastructure/mail/open_mail_app_facade.dart' as _i11;
-import '../infrastructure/media/media_facade.dart' as _i24;
-import '../infrastructure/settings/settings_facade.dart' as _i26;
+import '../infrastructure/media/media_facade.dart' as _i26;
+import '../infrastructure/settings/settings_facade.dart' as _i28;
 import '../infrastructure/settings/settings_mapper.dart' as _i15;
-import 'register_module.dart' as _i30; // ignore_for_file: unnecessary_lambdas
+import 'register_module.dart' as _i32; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -67,23 +69,25 @@ Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
   gh.factory<_i18.SignUpBloc>(() => _i18.SignUpBloc(get<_i8.IAuthFacade>()));
   gh.factory<_i19.AuthBloc>(
       () => _i19.AuthBloc(get<_i8.IAuthFacade>(), get<_i10.IMailFacade>()));
-  gh.factory<_i20.ForgotPasswordBloc>(
-      () => _i20.ForgotPasswordBloc(get<_i8.IAuthFacade>()));
-  gh.lazySingleton<_i21.ILocationFacade>(
-      () => _i22.LocationFacade(get<_i14.Location>()));
-  gh.lazySingleton<_i23.IMediaFacade>(() => _i24.MediaFacade(
+  gh.factory<_i20.CreateRequestBloc>(() => _i20.CreateRequestBloc(
+      get<_i8.IAuthFacade>(), get<_i21.IRequestFacade>()));
+  gh.factory<_i22.ForgotPasswordBloc>(
+      () => _i22.ForgotPasswordBloc(get<_i8.IAuthFacade>()));
+  gh.lazySingleton<_i23.ILocationFacade>(
+      () => _i24.LocationFacade(get<_i14.Location>()));
+  gh.lazySingleton<_i25.IMediaFacade>(() => _i26.MediaFacade(
       get<_i4.FirebaseAuth>(),
       get<_i6.FirebaseStorage>(),
       get<_i12.ImagePicker>()));
-  gh.lazySingleton<_i25.ISettingsFacade>(() => _i26.SettingsFacade(
+  gh.lazySingleton<_i27.ISettingsFacade>(() => _i28.SettingsFacade(
       get<_i16.SharedPreferences>(), get<_i15.SettingsMapper>()));
-  gh.factory<_i27.ProfileBloc>(() => _i27.ProfileBloc(get<_i8.IAuthFacade>(),
-      get<_i23.IMediaFacade>(), get<_i21.ILocationFacade>()));
-  gh.factory<_i28.SettingsBloc>(
-      () => _i28.SettingsBloc(get<_i25.ISettingsFacade>()));
+  gh.factory<_i29.ProfileBloc>(() => _i29.ProfileBloc(get<_i8.IAuthFacade>(),
+      get<_i25.IMediaFacade>(), get<_i23.ILocationFacade>()));
+  gh.factory<_i30.SettingsBloc>(
+      () => _i30.SettingsBloc(get<_i27.ISettingsFacade>()));
   return get;
 }
 
-class _$FirebaseInjectableModule extends _i29.FirebaseInjectableModule {}
+class _$FirebaseInjectableModule extends _i31.FirebaseInjectableModule {}
 
-class _$RegisterModule extends _i30.RegisterModule {}
+class _$RegisterModule extends _i32.RegisterModule {}
