540134cb375773aee0f9fbd0e0a9fb147dcb240a
David Michael II <knowdavidmichael@gmail.com>
2022-08-23 23:24:11 +0100

    fix: lint errors

--- lib/infrastructure/request/request_facade.dart
+++ lib/infrastructure/request/request_facade.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:injectable/injectable.dart';
 import 'package:jini/domain/core/value_object.dart';
 import 'package:dartz/dartz.dart';
@@ -10,10 +9,6 @@ import 'package:jini/infrastructure/request/request_dto.dart';
 
 @LazySingleton(as: IRequestFacade)
 class RequestFacade implements IRequestFacade {
-  RequestFacade(this._firestore, this._firebaseAuth);
-  final FirebaseFirestore _firestore;
-  final FirebaseAuth _firebaseAuth;
-
   @override
   Future<Either<RequestFailure, Unit>> createRequest({
     required Request request,
@@ -49,7 +44,6 @@ class RequestFacade implements IRequestFacade {
 
   @override
   Future<Either<RequestFailure, Unit>> deleteRequest(Uid uid) async {
-    // TODO: implement deleteRequest
     throw UnimplementedError();
   }
 }
