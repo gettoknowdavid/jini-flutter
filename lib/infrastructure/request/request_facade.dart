f6ca5fc146184f9e94072c73d2a92d9cc4c852ef
David Michael II <knowdavidmichael@gmail.com>
2022-08-23 23:22:08 +0100

    Update request_facade.dart

--- lib/infrastructure/request/request_facade.dart
+++ lib/infrastructure/request/request_facade.dart
@@ -2,12 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:injectable/injectable.dart';
 import 'package:jini/domain/core/value_object.dart';
-import 'package:jini/domain/auth/j_user.dart';
 import 'package:dartz/dartz.dart';
 import 'package:jini/domain/request/i_request_facade.dart';
 import 'package:jini/domain/request/request.dart';
 import 'package:jini/domain/request/request_failure.dart';
-import 'package:jini/infrastructure/auth/j_user_dtos.dart';
 import 'package:jini/infrastructure/request/request_dto.dart';
 
 @LazySingleton(as: IRequestFacade)
@@ -21,20 +19,14 @@ class RequestFacade implements IRequestFacade {
     required Request request,
   }) async {
     try {
-      // final userId = _firebaseAuth.currentUser!.uid;
-      // final user = await jUsersRef.doc(userId).get().then((v) => v.data);
+      final requestDto = RequestDto.fromDomain(request);
 
-      final requestDto = RequestDto(
-        uid: request.uid.getOrCrash()!,
-        user: JUserDto.fromDomain(request.user),
-        bloodGroup: request.user.bloodGroup!.getOrCrash()!,
-        createdAt: request.createdAt,
-      );
+      print(requestDto.toJson());
 
       final storeRequest = await requestsRef
           .whereFieldPath(
             FieldPath.fromString('bloodGroup'),
-            isEqualTo: requestDto.bloodGroup,
+            isEqualTo: requestDto.bloodGroup.name,
           )
           .get()
           .then((snap) => snap.docs.map((e) => e.data).toList());
@@ -43,7 +35,10 @@ class RequestFacade implements IRequestFacade {
         return left(const RequestFailure.alreadyExists());
       }
 
-      await requestsRef.doc(requestDto.uid).set(requestDto);
+      await requestsRef
+          .doc(requestDto.uid)
+          .set(requestDto)
+          .timeout(const Duration(seconds: 10));
 
       return right(unit);
     } on FirebaseException catch (e) {
