dfa9171fd18e626111f691e3126ed5e715360825
David Michael II <knowdavidmichael@gmail.com>
2022-08-23 19:00:18 +0100

    add isCreating boolean to CreateRequestState

--- lib/application/create_request/create_request_state.dart
+++ lib/application/create_request/create_request_state.dart
@@ -6,6 +6,7 @@ class CreateRequestState with _$CreateRequestState {
     required JUser user,
     required IBloodGroup bloodGroup,
     required DateTime timeAdded,
+    required bool isCreating,
     required bool showErrorMessages,
     required Option<Either<RequestFailure, Unit>> createOption,
   }) = _CreateRequestState;
@@ -14,6 +15,7 @@ class CreateRequestState with _$CreateRequestState {
         user: JUser.empty(),
         bloodGroup: IBloodGroup(null),
         timeAdded: DateTime.now(),
+        isCreating: false,
         showErrorMessages: false,
         createOption: none(),
       );
