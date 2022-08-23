bf806a075e9851d6d2e36f393f9ff7f7c2600e51
David Michael II <knowdavidmichael@gmail.com>
2022-08-23 23:22:11 +0100

    Update create_request_page.dart

--- lib/presentation/request/create_request_page.dart
+++ lib/presentation/request/create_request_page.dart
@@ -8,6 +8,7 @@ import 'package:jini/presentation/core/common/image_resources.dart';
 import 'package:jini/presentation/core/common/j_error_messages.dart';
 import 'package:jini/presentation/core/common/j_screen_util.dart';
 import 'package:jini/presentation/core/common/j_widget_styles.dart';
+import 'package:jini/presentation/core/widgets/j_button.dart';
 import 'package:jini/presentation/core/widgets/j_snackbars.dart';
 import 'package:jini/presentation/profile/widgets/blood_group_grid.dart';
 
@@ -49,64 +50,73 @@ class CreateRequestPage extends StatelessWidget {
             );
           },
           builder: (context, state) {
-            return Column(
-              children: [
-                Container(
-                  padding: JScreenUtil.padFromLTRB(4, 20, 2, 0),
-                  height: JScreenUtil.sh(0.3),
-                  width: JScreenUtil.sw(1),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(JScreenUtil.r(40)),
+            return Padding(
+              padding: JScreenUtil.padSymmetric(horizontal: 18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    padding: JScreenUtil.padOnly(top: 20),
+                    height: JScreenUtil.sh(0.3),
+                    width: JScreenUtil.sw(1),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(JScreenUtil.r(40)),
+                      ),
                     ),
+                    child: Image.asset(ImageResources.bloodDonation),
                   ),
-                  child: Image.asset(ImageResources.bloodDonation),
-                ),
-                JScreenUtil.vSpace(18),
-                Padding(
-                  padding: JScreenUtil.padSymmetric(horizontal: 18),
-                  child: Text(
+                  const Spacer(),
+                  Text(
                     'Select Blood group',
+                    textAlign: TextAlign.left,
                     style: TextStyle(
-                      fontSize: JScreenUtil.r(18),
+                      fontSize: JScreenUtil.r(22),
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                ),
-                JScreenUtil.hSpace(20),
-                GridView.builder(
-                  primary: false,
-                  shrinkWrap: true,
-                  itemCount: BloodGroup.values.length,
-                  padding: JScreenUtil.globalPadding,
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 3,
-                    mainAxisExtent: JScreenUtil.sh(0.08),
-                    crossAxisSpacing: JScreenUtil.r(10),
-                    mainAxisSpacing: JScreenUtil.r(10),
-                  ),
-                  itemBuilder: (context, i) {
-                    final _bg = BloodGroup.values[i];
-                    final _cBg = bloc.state.bloodGroup.getOrCrash();
-                    final selected = _cBg == null ? false : _bg == _cBg;
+                  JScreenUtil.vSpace(14),
+                  GridView.builder(
+                    primary: false,
+                    shrinkWrap: true,
+                    itemCount: BloodGroup.values.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisExtent: JScreenUtil.sh(0.08),
+                      crossAxisSpacing: JScreenUtil.r(10),
+                      mainAxisSpacing: JScreenUtil.r(10),
+                    ),
+                    itemBuilder: (context, i) {
+                      final _bg = BloodGroup.values[i];
+                      final _cBg = bloc.state.bloodGroup.getOrCrash();
+                      final selected = _cBg == null ? false : _bg == _cBg;
 
-                    return Parent(
-                      gesture: Gestures()
-                        ..onTap(() => bloc
-                            .add(CreateRequestEvent.bloodGroupSelected(_bg))),
-                      style: JWidgetStyles.selectItemsStyles(selected, theme),
-                      child: Text(
-                        _bg.value,
-                        style: textTheme.titleMedium?.copyWith(
-                          color: selected
-                              ? Colors.white
-                              : textTheme.bodyLarge?.color,
+                      return Parent(
+                        gesture: Gestures()
+                          ..onTap(() => bloc
+                              .add(CreateRequestEvent.bloodGroupSelected(_bg))),
+                        style: JWidgetStyles.selectItemsStyles(selected, theme),
+                        child: Text(
+                          _bg.value,
+                          style: textTheme.titleMedium?.copyWith(
+                            color: selected
+                                ? Colors.white
+                                : textTheme.bodyLarge?.color,
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ],
+                      );
+                    },
+                  ),
+                  const Spacer(),
+                  JButton(
+                    onPressed: () =>
+                        bloc.add(CreateRequestEvent.createPressed()),
+                    title: 'Create Request',
+                    loading: bloc.state.isCreating,
+                  ),
+                  JScreenUtil.vSpace(10),
+                ],
+              ),
             );
           },
         ),
