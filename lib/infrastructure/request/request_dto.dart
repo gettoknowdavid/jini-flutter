105f6d133deaf1d0324860a389c88f09d1ad0132
David Michael II <knowdavidmichael@gmail.com>
2022-08-23 23:22:03 +0100

    Update request_dto.dart

--- lib/infrastructure/request/request_dto.dart
+++ lib/infrastructure/request/request_dto.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:jini/domain/core/blood_group.dart';
 import 'package:jini/domain/request/request.dart';
-import 'package:jini/infrastructure/auth/j_user_dtos.dart';
+import 'package:jini/infrastructure/core/geo_point_converter.dart';
 
 part 'request_dto.freezed.dart';
 part 'request_dto.g.dart';
@@ -11,6 +11,7 @@ part 'request_dto.g.dart';
 const firestoreSerializable = JsonSerializable(
   explicitToJson: true,
   createFactory: false,
+  anyMap: true,
 );
 
 @freezed
@@ -18,7 +19,10 @@ const firestoreSerializable = JsonSerializable(
 class RequestDto with _$RequestDto {
   factory RequestDto({
     required String uid,
-    required JUserDto user,
+    required String userUid,
+    required String userName,
+    @GeoConverter() required GeoPoint userLocation,
+    String? userAvatar,
     required BloodGroup bloodGroup,
     required DateTime createdAt,
   }) = _RequestDto;
@@ -26,7 +30,13 @@ class RequestDto with _$RequestDto {
   factory RequestDto.fromDomain(Request request) {
     return RequestDto(
       uid: request.uid.getOrCrash()!,
-      user: JUserDto.fromDomain(request.user),
+      userUid: request.userUid.getOrCrash()!,
+      userName: request.userName.getOrCrash()!,
+      userLocation: GeoPoint(
+        request.userLocation.getOrCrash()!.latitude,
+        request.userLocation.getOrCrash()!.longitude,
+      ),
+      userAvatar: request.userAvatar,
       bloodGroup: request.bloodGroup.getOrCrash()!,
       createdAt: request.createdAt,
     );
