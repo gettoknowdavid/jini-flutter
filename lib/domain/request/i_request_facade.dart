4aac516923ed4ae4f9c0c3214309c5fc8876260e
David Michael II <knowdavidmichael@gmail.com>
2022-08-23 19:44:44 +0100

    fix: IRequestFacade arg

--- lib/domain/request/i_request_facade.dart
+++ lib/domain/request/i_request_facade.dart
@@ -1,12 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:jini/domain/auth/j_user.dart';
 import 'package:jini/domain/core/value_object.dart';
+import 'package:jini/domain/request/request.dart';
 import 'package:jini/domain/request/request_failure.dart';
 
 abstract class IRequestFacade {
   Future<Either<RequestFailure, Unit>> createRequest({
-    required JUser user,
-    required IBloodGroup bloodGroup,
+    required Request request,
   });
 
   Future<Either<RequestFailure, Unit>> deleteRequest(Uid uid);
